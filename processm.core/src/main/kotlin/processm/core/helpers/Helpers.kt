f2d788965280c66e3e62b9285cff127452dd8121
Tomasz Pawlak <tpawlak@cs.put.poznan.pl>
2020-02-05 14:41:47 +0100

    Debugging TeamCity build

--- processm.core/src/main/kotlin/processm/core/helpers/Helpers.kt
+++ processm.core/src/main/kotlin/processm/core/helpers/Helpers.kt
@@ -5,20 +5,20 @@ import processm.core.logging.*
 
 private const val EnvironmentVariablePrefix = "processm_"
 
-private class Helpers {}
+private object Helpers {}
 
 fun loadConfiguration() {
     // Load from environment variables processm_* by replacing _ with .
     System.getenv().filterKeys { it.startsWith(EnvironmentVariablePrefix, true) }.forEach {
         val key = it.key.replace("_", ".")
-        Helpers::class.logger().debug("Setting parameter $key=${it.value} using environment variable ${it.key}")
+        Helpers.logger().debug("Setting parameter $key=${it.value} using environment variable ${it.key}")
         System.setProperty(key, it.value)
     }
 
     // Load from configuration file, possibly overriding the environment settings
     Helpers::class.java.classLoader.getResourceAsStream("config.properties").use {
         Properties().apply { load(it) }.forEach {
-            Helpers::class.logger().debug("Setting parameter ${it.key}=${it.value} using config.properties")
+            Helpers.logger().debug("Setting parameter ${it.key}=${it.value} using config.properties")
             System.setProperty(it.key as String, it.value as String)
         }
     }
