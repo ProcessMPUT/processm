b7184895225827bdc28a179bea9497d5401f9880
dsroka <daniel.sroka@put.poznan.pl>
2020-02-13 11:09:35 +0100

    fix warnings in frontend-app

--- processm.webui/src/main/frontend-app/src/main.ts
+++ processm.webui/src/main/frontend-app/src/main.ts
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Vuetify from "vuetify";
 import vuetify from "./plugins/vuetify";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
