cd1e2a02738c4bb104b54893d8a44fbee4962720
Tomasz Pawlak <tpawlak@cs.put.poznan.pl>
2020-02-18 17:24:48 +0100

    chore: schema specification for timescaledb extension creation

--- processm.core/src/main/kotlin/processm/core/persistence/Migrator.kt
+++ processm.core/src/main/kotlin/processm/core/persistence/Migrator.kt
@@ -1,6 +1,7 @@
 package processm.core.persistence
 
 import org.flywaydb.core.Flyway
+import org.flywaydb.core.api.configuration.FluentConfiguration
 import processm.core.logging.enter
 import processm.core.logging.exit
 import processm.core.logging.logger
@@ -19,9 +20,22 @@ object Migrator {
 
         logger().debug("Migrating database if required")
         val url = System.getProperty("processm.core.persistence.connection.URL")
-        val flyway = Flyway.configure().dataSource(url, null, null).load()
+        val conf = Flyway.configure().dataSource(url, null, null)
+        applyDefaultSchema(conf, url)
+        val flyway = conf.load()
         flyway.migrate()
 
         logger().exit()
+    }
+
+    /**
+     * Workaround for a known bug in Flyway as of 2020-02-18.
+     * @see https://github.com/flyway/flyway/issues/2182
+     */
+    private fun applyDefaultSchema(conf: FluentConfiguration, url: String) {
+        val schema = Regex("defaultSchema=([^&]*)").find(url)?.groupValues?.get(1)
+        if (schema === null)
+            return
+        conf.schemas(schema)
     }
 }
