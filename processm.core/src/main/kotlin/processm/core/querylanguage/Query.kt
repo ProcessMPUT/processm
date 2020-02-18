7092e56feec8556698cf9689a2e5888d71a592a6
Tomasz Pawlak <tpawlak@cs.put.poznan.pl>
2020-02-18 09:39:15 +0100

    chore: removed unused test code

--- processm.core/src/main/kotlin/processm/core/querylanguage/Query.kt
+++ processm.core/src/main/kotlin/processm/core/querylanguage/Query.kt
@@ -2,6 +2,8 @@ package processm.core.querylanguage
 
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
+import java.text.NumberFormat
+import java.util.*
 
 /**
  * Represents log query as parsed from the string given as constructor argument.
@@ -20,14 +22,5 @@ class Query(val query: String) {
         var tokens = CommonTokenStream(lexer)
         val parser = QueryLanguage(tokens)
         val tree = parser.query()
-    }
-}
-
-fun test() {
-    val qName = "s"
-    when {
-        qName.equals("extension", true) -> TODO()
-        qName.equals("global", true) -> TODO()
-        qName.equals("classifier", true) -> TODO()
     }
 }
