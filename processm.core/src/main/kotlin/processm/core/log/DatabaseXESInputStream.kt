dbdc1438529c4b854284c75398c22d90e59ee16e
Bartosz Górka <bartosz.gorka@interia.eu>
2020-02-19 16:19:09 +0100

    feat: Read XES objects from the database

--- processm.core/src/main/kotlin/processm/core/log/DatabaseXESInputStream.kt
+++ processm.core/src/main/kotlin/processm/core/log/DatabaseXESInputStream.kt
@@ -1,14 +1,460 @@
 package processm.core.log
 
-class DatabaseXESInputStream(private val query: Any) : XESInputStream {
+import processm.core.log.attribute.*
+import processm.core.persistence.DBConnectionPool
+import java.sql.Connection
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+
+class DatabaseXESInputStream(private val logId: Int) : XESInputStream {
     override fun iterator(): Iterator<XESElement> = sequence<XESElement> {
-        TODO(
-            "Find and fetch XES elements from database. This function is called for the first element being fetched " +
-                    "from this sequence and then suspended on the first yield() statement. By fetching a next element from " +
-                    "this sequence, this function is resumed and processing advances toward the next yield statement."
-        )
-//        yield(Log()) // Return Log element
-//        yield(Trace()) // Return Trace
-//        yield(Event()) // Return Event
+        val connection = openReadOnlyConnection()
+
+        // Get log structure
+        val log = parseLog(connection, getLogStatement(connection, logId)) ?: return@sequence
+
+        // Return log structure
+        yield(log)
+
+        // Prepare traces statement
+        val tracesExecutedQuery = getTracesStatement(connection, logId).executeQuery()
+        while (tracesExecutedQuery.next()) {
+            val (traceId, trace) = parseTrace(connection, tracesExecutedQuery)
+
+            // Return trace
+            yield(trace)
+
+            // Prepare events statement
+            val eventsExecutedQuery = getEventsStatement(connection, traceId).executeQuery()
+            while (eventsExecutedQuery.next()) {
+                // Return event
+                yield(parseEvent(connection, eventsExecutedQuery))
+            }
+        }
     }.iterator()
+
+    private fun openReadOnlyConnection(): Connection {
+        return DBConnectionPool.getConnection().apply {
+            autoCommit = false
+            prepareStatement("START TRANSACTION READ ONLY").execute()
+        }
+    }
+
+    private fun getLogStatement(connection: Connection, logId: Int): PreparedStatement {
+        with(
+            connection.prepareStatement(
+                """
+            SELECT
+                features,
+                "concept:name",
+                "identity:id",
+                "lifecycle:model"
+            FROM
+                logs
+            WHERE
+                id = ?
+            LIMIT 1;
+        """.trimIndent()
+            )
+        ) {
+            setInt(1, logId)
+
+            return this
+        }
+    }
+
+    private fun getClassifiersStatement(connection: Connection, logId: Int): PreparedStatement {
+        with(
+            connection.prepareStatement(
+                """
+            SELECT
+                scope,
+                name,
+                keys
+            FROM
+                classifiers
+            WHERE
+                log_id = ?
+            ORDER BY id;
+        """.trimIndent()
+            )
+        ) {
+            setInt(1, logId)
+
+            return this
+        }
+    }
+
+    private fun getExtensionsStatement(connection: Connection, logId: Int): PreparedStatement {
+        with(
+            connection.prepareStatement(
+                """
+            SELECT
+                name,
+                prefix,
+                uri
+            FROM
+                extensions
+            WHERE
+                log_id = ?
+            ORDER BY id;
+        """.trimIndent()
+            )
+        ) {
+            setInt(1, logId)
+
+            return this
+        }
+    }
+
+    private fun getGlobalsStatement(connection: Connection, logId: Int): PreparedStatement {
+        with(
+            connection.prepareStatement(
+                """
+            SELECT
+                id,
+                parent_id,
+                scope,
+                type,
+                key,
+                string_value,
+                date_value,
+                int_value,
+                bool_value,
+                real_value,
+                in_list_attr 
+            FROM
+                globals
+            WHERE
+                log_id = ?
+            ORDER BY id;
+        """.trimIndent()
+            )
+        ) {
+            setInt(1, logId)
+
+            return this
+        }
+    }
+
+    private fun getLogAttributesStatement(connection: Connection, logId: Int): PreparedStatement {
+        with(
+            connection.prepareStatement(
+                """
+            SELECT
+                id,
+                parent_id,
+                type,
+                key,
+                string_value,
+                date_value,
+                int_value,
+                bool_value,
+                real_value,
+                in_list_attr 
+            FROM
+                logs_attributes
+            WHERE
+                log_id = ?
+            ORDER BY id;
+        """.trimIndent()
+            )
+        ) {
+            setInt(1, logId)
+
+            return this
+        }
+    }
+
+    private fun getTracesStatement(connection: Connection, logId: Int): PreparedStatement {
+        with(
+            connection.prepareStatement(
+                """
+            SELECT
+                id,
+                "concept:name",
+                "cost:total",
+                "cost:currency",
+                "identity:id",
+                event_stream
+            FROM
+                traces
+            WHERE
+                log_id = ?
+            ORDER BY id;
+        """.trimIndent()
+            )
+        ) {
+            setInt(1, logId)
+
+            return this
+        }
+    }
+
+    private fun getTraceAttributesStatement(connection: Connection, traceId: Long): PreparedStatement {
+        with(
+            connection.prepareStatement(
+                """
+            SELECT
+                id,
+                parent_id,
+                type,
+                key,
+                string_value,
+                date_value,
+                int_value,
+                bool_value,
+                real_value,
+                in_list_attr 
+            FROM
+                traces_attributes
+            WHERE
+                trace_id = ?
+            ORDER BY id;
+        """.trimIndent()
+            )
+        ) {
+            setLong(1, traceId)
+
+            return this
+        }
+    }
+
+    private fun getEventsStatement(connection: Connection, traceId: Long): PreparedStatement {
+        with(
+            connection.prepareStatement(
+                """
+            SELECT
+                id,
+                "concept:name",
+                "concept:instance",
+                "cost:total",
+                "cost:currency",
+                "identity:id",
+                "lifecycle:transition",
+                "lifecycle:state",
+                "org:resource",
+                "org:role",
+                "org:group",
+                "time:timestamp"
+            FROM
+                events
+            WHERE
+                trace_id = ?
+            ORDER BY id;
+        """.trimIndent()
+            )
+        ) {
+            setLong(1, traceId)
+
+            return this
+        }
+    }
+
+    private fun getEventAttributesStatement(connection: Connection, eventId: Long): PreparedStatement {
+        with(
+            connection.prepareStatement(
+                """
+            SELECT
+                id,
+                parent_id,
+                type,
+                key,
+                string_value,
+                date_value,
+                int_value,
+                bool_value,
+                real_value,
+                in_list_attr 
+            FROM
+                events_attributes
+            WHERE
+                event_id = ?
+            ORDER BY id;
+        """.trimIndent()
+            )
+        ) {
+            setLong(1, eventId)
+
+            return this
+        }
+    }
+
+    private fun parseLog(connection: Connection, query: PreparedStatement): Log? {
+        val resultSet = query.executeQuery()
+        if (!resultSet.next())
+            return null
+
+        with(Log()) {
+            features = resultSet.getString("features")
+            conceptName = resultSet.getString("concept:name")
+            identityId = resultSet.getString("identity:id")
+            lifecycleModel = resultSet.getString("lifecycle:model")
+
+            // Load classifiers, extensions, globals and attributes inside log structure
+            parseClassifiers(getClassifiersStatement(connection, logId), this)
+            parseExtensions(getExtensionsStatement(connection, logId), this)
+            parseGlobals(getGlobalsStatement(connection, logId), this)
+            parseLogAttributes(getLogAttributesStatement(connection, logId), this)
+
+            return this
+        }
+    }
+
+    private fun parseClassifiers(query: PreparedStatement, log: Log): Log {
+        val resultSet = query.executeQuery()
+        while (resultSet.next()) {
+            with(resultSet) {
+                val name = getString("name")
+                val classifier = Classifier(name, keys = getString("keys"))
+
+                when (getString("scope")) {
+                    "event" ->
+                        log.eventClassifiersInternal[name] = classifier
+                    "trace" ->
+                        log.traceClassifiersInternal[name] = classifier
+                    else ->
+                        throw IllegalStateException("Can not assign classifier with scope ${getString("scope")}")
+                }
+            }
+        }
+
+        return log
+    }
+
+    private fun parseExtensions(query: PreparedStatement, log: Log): Log {
+        val resultSet = query.executeQuery()
+        while (resultSet.next()) {
+            with(resultSet) {
+                val prefix = getString("prefix")
+                val extension = Extension(name = getString("name"), prefix = prefix, uri = getString("uri"))
+
+                log.extensionsInternal[prefix] = extension
+            }
+        }
+
+        return log
+    }
+
+    private fun parseGlobals(query: PreparedStatement, log: Log): Log {
+        val resultSet = query.executeQuery()
+        val fn = { r: ResultSet, k: String -> r.getInt(k) }
+
+        if (!resultSet.next()) return log
+
+        while (!resultSet.isAfterLast) {
+            val scope = resultSet.getString("scope")
+            val attribute = parseRecordsIntoAttributes(resultSet, fn)
+
+            when (scope) {
+                "event" ->
+                    log.eventGlobalsInternal[attribute.key] = attribute
+                "trace" ->
+                    log.traceGlobalsInternal[attribute.key] = attribute
+                else ->
+                    throw IllegalStateException("Can not assign global attribute with scope $scope")
+            }
+        }
+
+        return log
+    }
+
+    private fun parseLogAttributes(query: PreparedStatement, element: Log) =
+        parseAttributes(query, { r: ResultSet, k: String -> r.getInt(k) }, element)
+
+    private fun parseTrace(connection: Connection, resultSet: ResultSet): Pair<Long, Trace> {
+        with(Trace()) {
+            conceptName = resultSet.getString("concept:name")
+            costCurrency = resultSet.getString("cost:currency")
+            costTotal = resultSet.getDouble("cost:total")
+            identityId = resultSet.getString("identity:id")
+            isEventStream = resultSet.getBoolean("event_stream")
+            val traceId = resultSet.getLong("id")
+
+            parseTracesEventsAttributes(getTraceAttributesStatement(connection, traceId), this)
+
+            return Pair(traceId, this)
+        }
+    }
+
+    private fun parseEvent(connection: Connection, resultSet: ResultSet): Event {
+        with(Event()) {
+            conceptName = resultSet.getString("concept:name")
+            conceptInstance = resultSet.getString("concept:instance")
+            costTotal = resultSet.getDouble("cost:total")
+            costCurrency = resultSet.getString("cost:currency")
+            identityId = resultSet.getString("identity:id")
+            lifecycleState = resultSet.getString("lifecycle:state")
+            lifecycleTransition = resultSet.getString("lifecycle:transition")
+            orgRole = resultSet.getString("org:role")
+            orgGroup = resultSet.getString("org:group")
+            orgResource = resultSet.getString("org:resource")
+            timeTimestamp = resultSet.getTimestamp("time:timestamp")
+
+            parseTracesEventsAttributes(getEventAttributesStatement(connection, resultSet.getLong("id")), this)
+
+            return this
+        }
+    }
+
+    private fun parseTracesEventsAttributes(query: PreparedStatement, element: XESElement) =
+        parseAttributes(query, { r: ResultSet, k: String -> r.getLong(k) }, element)
+
+    private fun parseAttributes(
+        query: PreparedStatement,
+        fn: (ResultSet, String) -> Number,
+        element: XESElement
+    ): XESElement {
+        val resultSet = query.executeQuery()
+
+        if (!resultSet.next()) return element
+
+        while (!resultSet.isAfterLast) {
+            with(parseRecordsIntoAttributes(resultSet, fn)) {
+                element.attributesInternal[this.key] = this
+            }
+        }
+
+        return element
+    }
+
+    private fun parseRecordsIntoAttributes(resultSet: ResultSet, fn: (ResultSet, String) -> Number): Attribute<*> {
+        val attr = attributeFromRecord(resultSet.getString("key"), resultSet)
+        val attrId = fn(resultSet, "id")
+
+        if (!resultSet.next()) return attr
+
+        if (fn(resultSet, "parent_id") != attrId) {
+            return attr
+        } else {
+            do {
+                val isInsideList = resultSet.getBoolean("in_list_attr")
+                with(parseRecordsIntoAttributes(resultSet, fn)) {
+                    if (isInsideList) {
+                        assert(attr is ListAttr)
+                        (attr as ListAttr).valueInternal.add(this)
+                    } else {
+                        attr.childrenInternal[this.key] = this
+                    }
+                }
+            } while (fn(resultSet, "parent_id") == attrId)
+        }
+
+        return attr
+    }
+
+    private fun attributeFromRecord(key: String, record: ResultSet): Attribute<*> {
+        with(record) {
+            return when (getString("type")) {
+                "int" -> IntAttr(key, getLong("int_value"))
+                "id" -> IDAttr(key, getString("string_value"))
+                "string" -> StringAttr(key, getString("string_value"))
+                "bool" -> BoolAttr(key, getBoolean("bool_value"))
+                "float" -> RealAttr(key, getDouble("real_value"))
+                "date" -> DateTimeAttr(key, getTimestamp("date_value"))
+                "list" -> ListAttr(key)
+                else -> throw IllegalStateException("Invalid attribute type stored in the database")
+            }
+        }
+    }
 }
