518914e64d9cb7af4f43d39c4d5ac9f7c72b0b54
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2020-02-29 00:40:27 +0100

    feat: (processm.core.models.bpmn.BPMNXMLService) towards lenient parser (#13)

--- processm.core/src/main/kotlin/processm/core/models/bpmn/BPMNXMLService.kt
+++ processm.core/src/main/kotlin/processm/core/models/bpmn/BPMNXMLService.kt
@@ -1,51 +1,111 @@
 package processm.core.models.bpmn
 
+import org.xml.sax.Attributes
+import org.xml.sax.helpers.AttributesImpl
 import processm.core.models.bpmn.jaxb.TDefinitions
 import java.io.InputStream
 import java.io.OutputStream
 import javax.xml.bind.JAXBContext
 import javax.xml.bind.JAXBElement
-import javax.xml.stream.XMLEventFactory
+import javax.xml.bind.UnmarshallerHandler
+import javax.xml.namespace.QName
 import javax.xml.stream.XMLEventReader
 import javax.xml.stream.XMLInputFactory
 import javax.xml.stream.XMLOutputFactory
-import javax.xml.stream.events.Namespace
-import javax.xml.stream.events.XMLEvent
+import javax.xml.stream.events.*
 
 
 sealed class BPMNXMLService {
 
-    private class NamespaceAddingEventReader(val base: XMLEventReader) : XMLEventReader by base {
+    private class Processor(val handler: UnmarshallerHandler, val reader: XMLEventReader) {
+        fun run(): Any? {
+            while (reader.hasNext())
+                process(reader.nextEvent())
+            return handler.result
+        }
 
-        private val factory = XMLEventFactory.newInstance();
+        private fun process(event: XMLEvent) {
+            if (event.isStartDocument)
+                handler.startDocument()
+            else if (event.isStartElement)
+                startElement(event.asStartElement())
+            else if (event.isEndElement)
+                endElement(event.asEndElement())
+            else if (event.isEndDocument)
+                handler.endDocument()
+            else if (event.isCharacters)
+                characters(event.asCharacters())
+            else
+                println("${event.eventType} || " + event.javaClass)
+        }
 
-        private fun process(event: XMLEvent): XMLEvent {
-            if (event.isStartElement) {
-                val orig = event.asStartElement()
-                if (orig.getNamespaceURI("bpmn") == null) {
-                    val namespaces = ArrayList<Namespace>()
-                    (orig.namespaces as Iterator<Namespace>).forEach { n -> namespaces.add(n) }
-                    namespaces.add(factory.createNamespace("bpmn", "http://www.omg.org/spec/BPMN/20100524/MODEL"))
-                    return factory.createStartElement(orig.name, orig.attributes, namespaces.iterator())
-                }
+        private val buffer = StringBuffer()
+
+        fun characters(element: Characters) {
+            buffer.append(element.data)
+            if (!reader.hasNext() || !reader.peek().isCharacters) {
+                handler.characters(buffer.toString().toCharArray(), 0, buffer.length)
+                buffer.delete(0, buffer.length)
             }
-            return event
         }
 
-        override fun nextEvent(): XMLEvent {
-            return process(base.nextEvent())
+        fun qname(name: QName): String {
+            return if (!name.prefix.isEmpty())
+                name.prefix + ":" + name.localPart
+            else
+                name.localPart
         }
 
-        override fun peek(): XMLEvent {
-            return process(base.peek())
+        fun endElement(element: EndElement) {
+            handler.endElement(
+                element.name.namespaceURI,
+                element.name.localPart,
+                qname(element.name)
+            )
+            (element.namespaces as Iterator<Namespace>).forEach { handler.endPrefixMapping(it.prefix) }
+        }
+
+        fun startElement(element: StartElement) {
+            (element.namespaces as Iterator<Namespace>).forEach {
+                handler.startPrefixMapping(
+                    it.prefix,
+                    it.namespaceURI
+                )
+            }
+            try {
+                handler.startElement(
+                    element.name.namespaceURI,
+                    element.name.localPart,
+                    qname(element.name),
+                    attributes(element)
+                )
+            } catch (e: NumberFormatException) {
+                println(e)
+            } catch (e: IllegalArgumentException) {
+                println(e)
+            }
+        }
+
+        fun attributes(event: StartElement): Attributes? {
+            val result = AttributesImpl()
+            val i: Iterator<*> = event.attributes
+            while (i.hasNext()) {
+                val staxAttr = i.next() as Attribute
+                val name = staxAttr.name
+                var qName = qname(name)
+                val type = staxAttr.dtdType
+                val value = staxAttr.value
+                result.addAttribute(name.namespaceURI, name.localPart, qName, type, value)
+            }
+            return result
         }
     }
 
     companion object {
         internal fun load(inp: InputStream): TDefinitions {
             val u = JAXBContext.newInstance(TDefinitions::class.java).createUnmarshaller();
-            val reader = NamespaceAddingEventReader(XMLInputFactory.newInstance().createXMLEventReader(inp))
-            return (u.unmarshal(reader) as JAXBElement<TDefinitions>).value
+            val reader = XMLInputFactory.newInstance().createXMLEventReader(inp)
+            return (Processor(u.unmarshallerHandler, reader).run() as JAXBElement<TDefinitions>).value
         }
 
         internal fun save(def: TDefinitions, out: OutputStream) {
