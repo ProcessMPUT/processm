cf06569aa60d6c12a9c13d76c4bbfdefc410bb44
Jędrzej Potoniec <jpotoniec@cs.put.poznan.pl>
2020-03-12 17:00:18 +0100

    chore: (processm.miners.heuristicminer.FromModelToLogAndBackAgain) cleaning-up (#10)

--- processm.miners/src/test/kotlin/processm/miners/heuristicminer/FromModelToLogAndBackAgain.kt
+++ processm.miners/src/test/kotlin/processm/miners/heuristicminer/FromModelToLogAndBackAgain.kt
@@ -6,10 +6,11 @@ import processm.core.models.causalnet.Node
 import processm.core.models.causalnet.causalnet
 import processm.core.models.causalnet.mock.Event
 import processm.core.models.causalnet.verifier.Verifier
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
+import processm.miners.heuristicminer.avoidability.ValidSequenceBasedAvoidabilityChecker
+import processm.miners.heuristicminer.longdistance.BruteForceLongDistanceDependencyMiner
+import processm.miners.heuristicminer.longdistance.LongDistanceDependencyMiner
+import processm.miners.heuristicminer.longdistance.NaiveLongDistanceDependencyMiner
+import kotlin.test.*
 
 class FromModelToLogAndBackAgain {
     val a = Node("a")
@@ -34,7 +35,7 @@ class FromModelToLogAndBackAgain {
         return inp.map { seq -> seq.map { it.activity } }.toString()
     }
 
-    private fun test(reference: Model) {
+    private fun test(reference: Model, longDistanceDependencyMiner: LongDistanceDependencyMiner) {
         logger().debug("REFERENCE:\n${reference}")
         val referenceVerifier = Verifier(reference)
         val expectedSequences =
@@ -46,7 +47,7 @@ class FromModelToLogAndBackAgain {
             .validSequences
             .flatMap { seq -> List(1) { seq.asSequence().map { ab -> Event(ab.a.activity) } }.asSequence() }
         log.forEach { println(it.toList()) }
-        val hm = HeuristicMiner(log)
+        val hm = HeuristicMiner(log, longDistanceDependencyMiner = longDistanceDependencyMiner)
         val v = Verifier(hm.result)
 
         val actualSequences = v.validSequences.map { seq -> seq.map { ab -> ab.a }.filter { !it.special } }.toSet()
@@ -59,41 +60,58 @@ class FromModelToLogAndBackAgain {
         assertTrue(v.isSound)
     }
 
-    @Test
-    fun `hell of long-term dependencies`() {
-        test(causalnet {
-            start = a
-            end = e
-            a splits b1 or b2 or b1 + b2
-            b1 splits c + e or c + d
-            b2 splits c + e or c + d
-            c splits d or e
-            d splits e
-            a joins b1
-            a joins b2
-            b1 or b2 or b1 + b2 join c
-            b1 + b2 + c join d
-            c + b1 or c + b2 or d join e
-        })
+    private val hell = causalnet {
+        start = a
+        end = e
+        a splits b1 or b2 or b1 + b2
+        b1 splits c + e or c + d
+        b2 splits c + e or c + d
+        c splits d or e
+        d splits e
+        a joins b1
+        a joins b2
+        b1 or b2 or b1 + b2 join c
+        b1 + b2 + c join d
+        c + b1 or c + b2 or d join e
     }
 
     @Test
-    fun `reversed hell of long-term dependencies`() {
-        test(causalnet {
-            start = a
-            end = e
-            a splits c + d1 or b or c + d2
-            b splits d1 + c + d2
-            c splits d1 or d2 or d1 + d2
-            d1 splits e
-            d2 splits e
-            a joins b
-            a or b join c
-            b + c or a + c join d1
-            b + c or a + c join d2
-            d1 or d2 or d1 + d2 join e
-        })
+    fun `Brute - hell of long-term dependencies`() {
+        test(hell, BruteForceLongDistanceDependencyMiner(ValidSequenceBasedAvoidabilityChecker()))
     }
+
+    @Test
+    @Ignore
+    fun `Naive - hell of long-term dependencies`() {
+        test(hell, NaiveLongDistanceDependencyMiner())
+    }
+
+    private val reversedHell = causalnet {
+        start = a
+        end = e
+        a splits c + d1 or b or c + d2
+        b splits d1 + c + d2
+        c splits d1 or d2 or d1 + d2
+        d1 splits e
+        d2 splits e
+        a joins b
+        a or b join c
+        b + c or a + c join d1
+        b + c or a + c join d2
+        d1 or d2 or d1 + d2 join e
+    }
+
+    @Test
+    fun `Brute - reversed hell of long-term dependencies`() {
+        test(reversedHell, BruteForceLongDistanceDependencyMiner(ValidSequenceBasedAvoidabilityChecker()))
+    }
+
+    @Test
+    @Ignore("Naive approach is too simple")
+    fun `Naive - reversed hell of long-term dependencies`() {
+        test(reversedHell, NaiveLongDistanceDependencyMiner())
+    }
+
 
     /**
      * Intended sequences:
@@ -103,24 +121,32 @@ class FromModelToLogAndBackAgain {
      * a c d1 e
      * a c d2 e
      */
+    private val treachery = causalnet {
+        start = a
+        end = e
+        a splits b1 + e or b2 + e or c + d1 + d2 + b1 + b2 or c + d1 + e or c + d2 + e
+        b1 splits c
+        b2 splits c
+        c splits d1 or e or d2 or d1 + d2
+        d1 splits e
+        d2 splits e
+        a joins b1
+        a joins b2
+        a or b1 or b2 or a + b1 + b2 join c
+        a + c join d1
+        a + c join d2
+        a + c or d1 + d2 or a + d1 or a + d2 join e
+    }
+
     @Test
-    fun `treachery 9th circle of hell`() {
-        test(causalnet {
-            start = a
-            end = e
-            a splits b1 + e or b2 + e or c + d1 + d2 + b1 + b2 or c + d1 + e or c + d2 + e
-            b1 splits c
-            b2 splits c
-            c splits d1 or e or d2 or d1 + d2
-            d1 splits e
-            d2 splits e
-            a joins b1
-            a joins b2
-            a or b1 or b2 or a + b1 + b2 join c
-            a + c join d1
-            a + c join d2
-            a + c or d1 + d2 or a + d1 or a + d2 join e
-        })
+    fun `Brute - treachery 9th circle of hell`() {
+        test(treachery, BruteForceLongDistanceDependencyMiner(ValidSequenceBasedAvoidabilityChecker()))
+    }
+
+    @Test
+    @Ignore("Naive approach is too simple")
+    fun `Naive - treachery 9th circle of hell`() {
+        test(treachery, NaiveLongDistanceDependencyMiner())
     }
 
     /**
@@ -128,84 +154,109 @@ class FromModelToLogAndBackAgain {
      *
      * Without mining long-term dependencies, heuristic miner is not able to even produce a sound model
      */
-    @Test
-    fun `sequential counting`() {
-        test(causalnet {
-            start = a
-            end = e
-            a splits b1
-            b1 splits b2 or c + e
-            b2 splits b3 or c + e
-            b3 splits c + e
-            c splits d1
-            d1 splits d2 or e
-            d2 splits d3 or e
-            d3 splits e
-            a joins b1
-            b1 joins b2
-            b2 joins b3
-            b1 or b2 or b3 join c
-            c joins d1
-            d1 joins d2
-            d2 joins d3
-            b1 + d1 or b2 + d2 or b3 + d3 join e
-        })
+    private val sequentialCounting = causalnet {
+        start = a
+        end = e
+        a splits b1
+        b1 splits b2 or c + e
+        b2 splits b3 or c + e
+        b3 splits c + e
+        c splits d1
+        d1 splits d2 or e
+        d2 splits d3 or e
+        d3 splits e
+        a joins b1
+        b1 joins b2
+        b2 joins b3
+        b1 or b2 or b3 join c
+        c joins d1
+        d1 joins d2
+        d2 joins d3
+        b1 + d1 or b2 + d2 or b3 + d3 join e
     }
+
+    @Test
+    fun `Brute - sequential counting`() {
+        test(sequentialCounting, BruteForceLongDistanceDependencyMiner(ValidSequenceBasedAvoidabilityChecker()))
+    }
+
+    @Test
+    fun `Naive - sequential counting`() {
+        test(sequentialCounting, NaiveLongDistanceDependencyMiner())
+    }
+
 
     /**
     Any non-zero number X of bs may be executed in parallel, but then dX must be executed (e.g., if exactly 2 (any) bs were executed, then d2 must be executed)
      */
+    private val numberIndicator = causalnet {
+        start = a
+        end = e
+        a splits b1 + d1 or b2 + d1 or b3 + d1
+        a splits b1 + b2 + d2 or b1 + b3 + d2 or b2 + b3 + d2
+        a splits b1 + b2 + b3 + d3
+        b1 splits c + d1 or c + d2 or c + d3
+        b2 splits c + d1 or c + d2 or c + d3
+        b3 splits c + d1 or c + d2 or c + d3
+        c splits d1 or d2 or d3
+        d1 splits e
+        d2 splits e
+        d3 splits e
+        a joins b1
+        a joins b2
+        a joins b3
+        b1 or b2 or b3 or b1 + b2 or b1 + b3 or b2 + b3 or b1 + b2 + b3 join c
+        a + b1 + c or a + b2 + c or a + b3 + c join d1
+        a + b1 + b2 + c or a + b1 + b3 + c or a + b2 + b3 + c join d2
+        a + b1 + b2 + b3 + c join d3
+        d1 or d2 or d3 join e
+    }
+
     @Test
-    fun `number indicator`() {
-        test(causalnet {
-            start = a
-            end = e
-            a splits b1 + d1 or b2 + d1 or b3 + d1
-            a splits b1 + b2 + d2 or b1 + b3 + d2 or b2 + b3 + d2
-            a splits b1 + b2 + b3 + d3
-            b1 splits c + d1 or c + d2 or c + d3
-            b2 splits c + d1 or c + d2 or c + d3
-            b3 splits c + d1 or c + d2 or c + d3
-            c splits d1 or d2 or d3
-            d1 splits e
-            d2 splits e
-            d3 splits e
-            a joins b1
-            a joins b2
-            a joins b3
-            b1 or b2 or b3 or b1 + b2 or b1 + b3 or b2 + b3 or b1 + b2 + b3 join c
-            a + b1 + c or a + b2 + c or a + b3 + c join d1
-            a + b1 + b2 + c or a + b1 + b3 + c or a + b2 + b3 + c join d2
-            a + b1 + b2 + b3 + c join d3
-            d1 or d2 or d3 join e
-        })
+    @Ignore("Hypothesis space is too small, as the underlying model also depends on a task not being executed")
+    fun `Brute - number indicator`() {
+        test(numberIndicator, BruteForceLongDistanceDependencyMiner(ValidSequenceBasedAvoidabilityChecker()))
+    }
+
+    @Ignore("Naive approach is too simple")
+    @Test
+    fun `Naive - number indicator`() {
+        test(numberIndicator, NaiveLongDistanceDependencyMiner())
     }
 
     /**
      * First N bs is executed in parallel, then first N ds is to be executed in parallel.
      */
+    private val firstNInParallel = causalnet {
+        start = a
+        end = e
+        a splits b1 or b1 + b2 or b1 + b2 + b3
+        b1 splits c + d1
+        b2 splits c + d2
+        b3 splits c + d3
+        c splits d1 or d1 + d2 or d1 + d2 + d3
+        d1 splits e
+        d2 splits e
+        d3 splits e
+        a joins b1
+        a joins b2
+        a joins b3
+        b1 or b1 + b2 or b1 + b2 + b3 join c
+        b1 + c join d1
+        b2 + c join d2
+        b3 + c join d3
+        d1 or d1 + d2 or d1 + d2 + d3 join e
+    }
+
     @Test
-    fun `first n in parallel`() {
-        test(causalnet {
-            start = a
-            end = e
-            a splits b1 or b1 + b2 or b1 + b2 + b3
-            b1 splits c + d1
-            b2 splits c + d2
-            b3 splits c + d3
-            c splits d1 or d1 + d2 or d1 + d2 + d3
-            d1 splits e
-            d2 splits e
-            d3 splits e
-            a joins b1
-            a joins b2
-            a joins b3
-            b1 or b1 + b2 or b1 + b2 + b3 join c
-            b1 + c join d1
-            b2 + c join d2
-            b3 + c join d3
-            d1 or d1 + d2 or d1 + d2 + d3 join e
-        })
+    fun `Brute - first n in parallel`() {
+        test(firstNInParallel, BruteForceLongDistanceDependencyMiner(ValidSequenceBasedAvoidabilityChecker()))
+    }
+
+    @Ignore("Naive approach is too simple")
+    @Test
+    fun `Naive - first n in parallel`() {
+        test(firstNInParallel, NaiveLongDistanceDependencyMiner())
     }
 
     /**
@@ -213,81 +264,102 @@ class FromModelToLogAndBackAgain {
      * Both bs and ds are executed in parallel.
      * For example, a valid sequence is `a b3 b2 c d1 d2 e`
      */
-    @Test
-    fun `parallel counting`() {
-        test(causalnet {
-            start = a
-            end = e
-            a splits b1 or b2 or b3 or b1 + b2 + d2 or b1 + b3 + d2 or b2 + b3 + d2 or b1 + b2 + b3 + d2 + d3
-            b1 splits c
-            b2 splits c
-            b3 splits c
-            c splits d1 or d1 + d2 or d1 + d2 + d3
-            d1 splits e
-            d2 splits e
-            d3 splits e
-            a joins b1
-            a joins b2
-            a joins b3
-            b1 or b2 or b3 or b1 + b2 or b2 + b3 or b1 + b3 or b1 + b2 + b3 join c
-            c joins d1
-            a + c join d2
-            a + c join d3
-            d1 or d1 + d2 or d1 + d2 + d3 join e
-        })
+    private val parallelCounting = causalnet {
+        start = a
+        end = e
+        a splits b1 or b2 or b3 or b1 + b2 + d2 or b1 + b3 + d2 or b2 + b3 + d2 or b1 + b2 + b3 + d2 + d3
+        b1 splits c
+        b2 splits c
+        b3 splits c
+        c splits d1 or d1 + d2 or d1 + d2 + d3
+        d1 splits e
+        d2 splits e
+        d3 splits e
+        a joins b1
+        a joins b2
+        a joins b3
+        b1 or b2 or b3 or b1 + b2 or b2 + b3 or b1 + b3 or b1 + b2 + b3 join c
+        c joins d1
+        a + c join d2
+        a + c join d3
+        d1 or d1 + d2 or d1 + d2 + d3 join e
     }
 
     @Test
-    fun `binary to unary decoder`() {
-        test(causalnet {
-            start = a
-            end = e
-            a splits c or b0 or b1 or b1 + b0 or b2 or b2 + b0 or b2 + b1 or b2 + b1 + b0
-            b0 splits c + d1 or c + d3 or c + d5 or c + d7
-            b1 splits c + d2 or c + d3 or c + d6 or c + d7
-            b2 splits c + d4 or c + d5 or c + d6 or c + d7
-            c splits d0 or d1 or d2 or d3 or d4 or d5 or d6 or d7
-            d0 splits e
-            d1 splits e
-            d2 splits e
-            d3 splits e
-            d4 splits e
-            d5 splits e
-            d6 splits e
-            d7 splits e
-            a joins b0
-            a joins b1
-            a joins b2
-            a or b0 or b1 or b1 + b0 or b2 or b2 + b0 or b2 + b1 or b2 + b1 + b0 join c
-            c joins d0
-            b0 + c join d1
-            b1 + c join d2
-            b1 + b0 + c join d3
-            b2 + c join d4
-            b2 + b0 + c join d5
-            b2 + b1 + c join d6
-            b2 + b1 + b0 + c join d7
-            d0 or d1 or d2 or d3 or d4 or d5 or d6 or d7 join e
-        })
+    @Ignore("Takes too long. Also outside the hypothesis space?")
+    fun `Brute - parallel counting`() {
+        test(parallelCounting, BruteForceLongDistanceDependencyMiner(ValidSequenceBasedAvoidabilityChecker()))
+    }
+
+    /**
+     * Nodes b2, b1, b0 encode a binary number, while nodes d7, .., d0 encode the corresponding unary number.
+     * Very hard for mining, because the dependencies are not only on executing, but also on not executing some nodes, e.g.,
+     * d1 is executed if, and only if, b0 is executed, but b2 and b1 are not executed.
+     */
+    private val binaryToUnary = causalnet {
+        start = a
+        end = e
+        a splits c or b0 or b1 or b1 + b0 or b2 or b2 + b0 or b2 + b1 or b2 + b1 + b0
+        b0 splits c + d1 or c + d3 or c + d5 or c + d7
+        b1 splits c + d2 or c + d3 or c + d6 or c + d7
+        b2 splits c + d4 or c + d5 or c + d6 or c + d7
+        c splits d0 or d1 or d2 or d3 or d4 or d5 or d6 or d7
+        d0 splits e
+        d1 splits e
+        d2 splits e
+        d3 splits e
+        d4 splits e
+        d5 splits e
+        d6 splits e
+        d7 splits e
+        a joins b0
+        a joins b1
+        a joins b2
+        a or b0 or b1 or b1 + b0 or b2 or b2 + b0 or b2 + b1 or b2 + b1 + b0 join c
+        c joins d0
+        b0 + c join d1
+        b1 + c join d2
+        b1 + b0 + c join d3
+        b2 + c join d4
+        b2 + b0 + c join d5
+        b2 + b1 + c join d6
+        b2 + b1 + b0 + c join d7
+        d0 or d1 or d2 or d3 or d4 or d5 or d6 or d7 join e
+    }
+
+    @Ignore
+    @Test
+    fun `Brute - binary to unary decoder`() {
+        test(binaryToUnary, BruteForceLongDistanceDependencyMiner(ValidSequenceBasedAvoidabilityChecker()))
+    }
+
+    /**
+     * A simple test inspired by the paper "Flexible heuristic miner"
+     */
+    private val fhm = causalnet {
+        start = a
+        end = e
+        a splits b1 or b2
+        b1 splits c + d1
+        b2 splits c + d2
+        c splits d1 or d2
+        d1 splits e
+        d2 splits e
+        a joins b1
+        a joins b2
+        b1 or b2 join c
+        b1 + c join d1
+        b2 + c join d2
+        d1 or d2 join e
     }
 
     @Test
-    fun `flexible fuzzy miner long-term dependency example`() {
-        test(causalnet {
-            start = a
-            end = e
-            a splits b1 or b2
-            b1 splits c + d1
-            b2 splits c + d2
-            c splits d1 or d2
-            d1 splits e
-            d2 splits e
-            a joins b1
-            a joins b2
-            b1 or b2 join c
-            b1 + c join d1
-            b2 + c join d2
-            d1 or d2 join e
-        })
+    fun `Brute - flexible heurisitc miner long-distance dependency example`() {
+        test(fhm, BruteForceLongDistanceDependencyMiner(ValidSequenceBasedAvoidabilityChecker()))
+    }
+
+    @Test
+    fun `Naive - flexible heurisitc miner long-distance dependency example`() {
+        test(fhm, NaiveLongDistanceDependencyMiner())
     }
 }
