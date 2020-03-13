a12d5d62a8f4ab51c587ab39b1e2def5d8524d10
Jędrzej Potoniec <jpotoniec@cs.put.poznan.pl>
2020-03-13 12:06:32 +0100

    chore: (processm.core.verifiers.CausalNetVerificationReport) some additional doc (#53)

--- processm.core/src/main/kotlin/processm/core/verifiers/CausalNetVerificationReport.kt
+++ processm.core/src/main/kotlin/processm/core/verifiers/CausalNetVerificationReport.kt
@@ -15,7 +15,19 @@ class CausalNetVerificationReport internal constructor(verifier: CausalNetVerifi
         !verifier.hasDeadParts
     ) {
 
+    /**
+     * Compute possible extensions for a given valid sequence, according to Definition 3.11 in PM
+     */
     val validSequences: SequenceWithMemory<CausalNetSequence> = verifier.validSequences
 
+    /**
+     * The set of all valid sequences without loops. This set should never be infinite.
+     *
+     * Recall that a state is a multi-set of pending obligations.
+     * A loop in a sequence occurs if there is a state B such that there is a state A earlier in the sequence, such that
+     * both states contains exactly the same pending obligations ignoring the number of occurences (e.g., this is the case for {a} and {a,a})
+     * and B contains no less of each obligations than A.
+     * In simple terms, B is A plus something.
+     */
     val validLoopFreeSequences: SequenceWithMemory<CausalNetSequence> = verifier.validLoopFreeSequences
 }
