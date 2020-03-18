b72880905362b76781c9c228626cea413a84650a
Jędrzej Potoniec <jpotoniec@cs.put.poznan.pl>
2020-03-18 16:42:07 +0100

    chore (processm.miners.heuristicminer.CompareOfflineWithOnline) random tests (#10)

--- processm.miners/src/test/kotlin/processm/miners/heuristicminer/CompareOfflineWithOnline.kt
+++ processm.miners/src/test/kotlin/processm/miners/heuristicminer/CompareOfflineWithOnline.kt
@@ -1,35 +1,47 @@
 package processm.miners.heuristicminer
 
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+import processm.core.helpers.allPermutations
 import processm.core.log.hierarchical.Log
 import processm.core.log.hierarchical.Trace
 import processm.core.models.causalnet.Model
 import processm.core.models.causalnet.Node
+import processm.core.models.causalnet.RandomGenerator
 import processm.core.models.causalnet.causalnet
-import processm.core.models.causalnet.toJS
 import processm.core.verifiers.CausalNetVerifier
-import kotlin.test.*
+import kotlin.random.Random
+import kotlin.test.Ignore
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class CompareOfflineWithOnline {
 
+    private fun seqs(model: Model) =
+        CausalNetVerifier().verify(model)
+            .validLoopFreeSequences
+            .map { seq -> seq.map { ab -> ab.a }.filter { !it.special } }
+            .toSet()
 
-    private fun compare(log: Log) {
-        fun seqs(model: Model) =
-            CausalNetVerifier().verify(model)
-                .validLoopFreeSequences
-                .map { seq -> seq.map { ab -> ab.a }.filter { !it.special } }
-                .toSet()
-
-        val offlineModel = OfflineHeuristicMiner(log).result
+    private fun online(log: Log): Set<List<Node>> {
         val hm = HeuristicMiner()
         hm.processLog(log)
         val onlineModel = hm.result
-        println("OFF-LINE")
-        println(offlineModel)
-        offlineModel.toJS("/tmp/offline.html")
-        println("ON-LINE")
-        println(onlineModel)
-        onlineModel.toJS("/tmp/online.html")
-        assertEquals(seqs(offlineModel), seqs(onlineModel))
+        return seqs(onlineModel)
+    }
+
+    private fun compare(log: Log, permuteLog: Boolean = false) {
+        val offlineModel = OfflineHeuristicMiner(log).result
+        val offlineSeqs = seqs(offlineModel)
+        if (permuteLog) {
+            for (perm in log.traces.toList().allPermutations()) {
+                val onlineSeqs = online(Log(perm.asSequence()))
+                assertEquals(offlineSeqs, onlineSeqs)
+            }
+        } else {
+            val onlineSeqs = online(log)
+            assertEquals(offlineSeqs, onlineSeqs)
+        }
     }
 
     private fun logFromString(text: String): Log =
@@ -45,8 +57,13 @@ class CompareOfflineWithOnline {
         return Log(tmp.map { seq -> Trace(seq.asSequence().map { ab -> event(ab.a.activity) }) }.asSequence())
     }
 
-    private fun compare(text: String) = compare(logFromString(text))
-    private fun compare(model: Model) = compare(logFromModel(model))
+    private fun compare(text: String, permuteLog: Boolean = false) =
+        compare(logFromString(text), permuteLog = permuteLog)
+
+    private fun compare(model: Model, permuteLog: (Log) -> Boolean = { false }) {
+        val log = logFromModel(model)
+        compare(log, permuteLog = permuteLog(log))
+    }
 
     @Test
     fun `4 a b c d 4 a c b d`() {
@@ -113,7 +130,8 @@ class CompareOfflineWithOnline {
                 a c b d e f g
                 a b c d f e g
                 a c b d f e g
-            """.trimIndent()
+            """.trimIndent(),
+            true
         )
     }
 
@@ -123,7 +141,8 @@ class CompareOfflineWithOnline {
             """
                 a b c d
                 a c b d 
-            """.trimIndent()
+            """.trimIndent(),
+            true
         )
     }
 
@@ -176,5 +195,30 @@ class CompareOfflineWithOnline {
             c2 + d2 join e2
             e1 + e2 join f
         })
+    }
+
+    @TestFactory
+    fun factory(): List<DynamicTest> {
+        //these few are painful to wait for due to the verifier being slow
+        val verifierIsSlow = setOf(19, 54, 84, 95, 103, 188, 194, 195)
+        val temporarilyIgnored = setOf(94, 87, 66, 45)
+        val seeds = List(240) { it } - verifierIsSlow
+        return seeds.flatMap { seed ->
+            val log = logFromModel(RandomGenerator(Random(seed)).generate())
+            val offlineSeqs = seqs(OfflineHeuristicMiner(log).result)
+            if (log.traces.count() <= 4) {
+                log.traces.toList().allPermutations().mapIndexed { idx, traces ->
+                    DynamicTest.dynamicTest("$seed/$idx") {
+                        val onlineSeqs = online(Log(traces.asSequence()))
+                        assertEquals(offlineSeqs, onlineSeqs)
+                    }
+                }
+            } else {
+                listOf(DynamicTest.dynamicTest(seed.toString()) {
+                    val onlineSeqs = online(log)
+                    assertEquals(offlineSeqs, onlineSeqs)
+                })
+            }
+        }
     }
 }
