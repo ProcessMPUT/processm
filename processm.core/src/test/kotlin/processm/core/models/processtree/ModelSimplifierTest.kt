b8ab2351bb879f0f02000aa04efaffb365dfb075
Bartosz Górka <bartosz.gorka@interia.eu>
2020-03-18 13:58:43 +0100

    feat: Process tree model simplify - remove extra tau from sequences
    Reference processm-team/processm#16

---
+++ processm.core/src/test/kotlin/processm/core/models/processtree/ModelSimplifierTest.kt
@@ -0,0 +1,136 @@
+package processm.core.models.processtree
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class ModelSimplifierTest {
+    @Test
+    fun `Remove tau from sequence with two activities`() {
+        val tree = processTree {
+            Sequence(
+                SilentActivity(),
+                Activity("A"),
+                SilentActivity(),
+                SilentActivity(),
+                Activity("B"),
+                SilentActivity()
+            )
+        }
+
+        ModelSimplifier().reduceTauLeafs(tree)
+
+        with(tree.root!!) {
+            assertEquals(children.size, 2)
+
+            assertEquals(children[0], Activity("A"))
+            assertEquals(children[1], Activity("B"))
+        }
+
+        val expectedTree = processTree {
+            Sequence(
+                Activity("A"),
+                Activity("B")
+            )
+        }
+        assertTrue(tree.languageEqual(expectedTree))
+    }
+
+    @Test
+    fun `Remove tau and generate sequence with one activity`() {
+        val tree = processTree {
+            Sequence(
+                SilentActivity(),
+                Activity("C")
+            )
+        }
+
+        ModelSimplifier().reduceTauLeafs(tree)
+
+        with(tree.root!!) {
+            assertEquals(children.size, 1)
+
+            assertEquals(children[0], Activity("C"))
+        }
+
+        val expectedTree = processTree {
+            Sequence(
+                Activity("C")
+            )
+        }
+        assertTrue(tree.languageEqual(expectedTree))
+    }
+
+    @Test
+    fun `Can not remove tau if only child`() {
+        val tree = processTree {
+            Sequence(
+                SilentActivity()
+            )
+        }
+
+        ModelSimplifier().reduceTauLeafs(tree)
+
+        with(tree.root!!) {
+            assertEquals(children.size, 1)
+
+            assertEquals(children[0], SilentActivity())
+        }
+    }
+
+    @Test
+    fun `Remove tau from sequence but not from other operators`() {
+        val tree = processTree {
+            RedoLoop(
+                SilentActivity(),
+                Exclusive(
+                    SilentActivity(),
+                    Activity("A")
+                ),
+                Parallel(
+                    Activity("B"),
+                    SilentActivity(),
+                    Activity("C")
+                ),
+                Activity("D"),
+                Sequence(
+                    SilentActivity(),
+                    Activity("E"),
+                    Sequence(
+                        Activity("F"),
+                        Activity("G"),
+                        SilentActivity()
+                    )
+                )
+            )
+        }
+
+        ModelSimplifier().reduceTauLeafs(tree)
+
+        val expectedTree = processTree {
+            RedoLoop(
+                SilentActivity(),
+                Exclusive(
+                    SilentActivity(),
+                    Activity("A")
+                ),
+                Parallel(
+                    Activity("B"),
+                    SilentActivity(),
+                    Activity("C")
+                ),
+                Activity("D"),
+                Sequence(
+                    Activity("E"),
+                    Sequence(
+                        Activity("F"),
+                        Activity("G")
+                    )
+                )
+            )
+        }
+
+        assertEquals(tree.toString(), "⟲(τ,×(τ,A),∧(B,τ,C),D,→(E,→(F,G)))")
+        assertTrue(tree.languageEqual(expectedTree))
+    }
+}
