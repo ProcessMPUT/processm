406f262ffb749b765b0377af120e651b7a8690be
Jędrzej Potoniec <jpotoniec@cs.put.poznan.pl>
2020-03-23 18:05:47 +0100

    chore: (processm.miners.heuristicminer.traceregisters) cleaning and doc (#10)

--- processm.miners/src/main/kotlin/processm/miners/heuristicminer/traceregisters/AbstractTraceRegister.kt
+++ processm.miners/src/main/kotlin/processm/miners/heuristicminer/traceregisters/AbstractTraceRegister.kt
@@ -2,37 +2,43 @@ package processm.miners.heuristicminer.traceregisters
 
 import processm.core.models.causalnet.Binding
 import processm.core.models.causalnet.Node
-import processm.miners.heuristicminer.HeuristicMiner
 
 /**
  * An abstract trace register providing basic storage capabilities
  */
 abstract class AbstractTraceRegister : TraceRegister {
 
-    class HashMapWithDefault<K, V>(private val default: () -> V) : HashMap<K, V>() {
+    /**
+     * Map with [get] behaving like [getOrPut].
+     *
+     * Cannot call [getOrPut] directly, as it is an extension function which calls [get]
+     */
+    protected class HashMapWithDefault<K, V>(private val default: () -> V) : HashMap<K, V>() {
         override operator fun get(key: K): V {
             val result = super.get(key)
-            if (result == null) {
+            return if (result == null) {
                 val new = default()
                 this[key] = new
-                return new
-            } else {
-                return result
-            }
+                new
+            } else
+                result
         }
     }
 
-    protected val bindingCounter = HashMapWithDefault<Binding, HashSet<List<Node>>> { HashSet() }
+    /**
+     * Storage mapping bindings to relevant traces
+     */
+    protected val bindingCounter = HashMapWithDefault<Binding, HashSet<NodeTrace>> { HashSet() }
 
-    override fun removeAll(traces: Collection<List<Node>>) {
+    override fun removeAll(traces: Collection<NodeTrace>) {
         bindingCounter.values.forEach { it.removeAll(traces) }
     }
 
-    override operator fun get(binding: Binding): Set<List<Node>> {
+    override operator fun get(binding: Binding): Set<NodeTrace> {
         return bindingCounter[binding]
     }
 
-    override fun selectBest(selector: (Set<List<Node>>) -> Boolean): Set<Binding> {
+    override fun selectBest(selector: (Set<NodeTrace>) -> Boolean): Set<Binding> {
         return bindingCounter.filterValues(selector).keys
     }
 }
--- processm.miners/src/main/kotlin/processm/miners/heuristicminer/traceregisters/DifferentAdfixTraceRegister.kt
+++ processm.miners/src/main/kotlin/processm/miners/heuristicminer/traceregisters/DifferentAdfixTraceRegister.kt
@@ -7,24 +7,27 @@ import kotlin.math.max
 import kotlin.math.min
 
 /**
- * A trace register storing traces with different prefixes (for joins) and suffixes (for splits)
+ * A trace register storing traces with different prefixes (for joins) and suffixes (for splits), while ignoring cardinality and order in the prefix/suffix.
+ * The rationale is that it is only important what activities can possibly be executed before/after, not any particular order of execution.
  */
 class DifferentAdfixTraceRegister : AbstractTraceRegister() {
 
     override fun register(bindings: List<Binding>, nodeTrace: NodeTrace) {
         for (binding in bindings) {
-            require(binding is Split || binding is Join)
             if (binding is Split) {
                 val idx = nodeTrace.lastIndexOf(binding.source)
-                require(idx >= 0)
-                val suffix = nodeTrace.subList(idx, nodeTrace.size)
-                if (bindingCounter[binding].any { it.subList(max(it.size - suffix.size, 0), it.size) == suffix })
+                check(idx >= 0)
+                val suffix = nodeTrace.subList(idx, nodeTrace.size).toSet()
+                if (bindingCounter[binding].any {
+                        it.subList(max(it.size - suffix.size, 0), it.size).toSet() == suffix
+                    })
                     continue
             } else {
-                val idx = nodeTrace.indexOf((binding as Join).target)
-                require(idx >= 0)
-                val prefix = nodeTrace.subList(0, idx)
-                if (bindingCounter[binding].any { it.subList(0, min(idx, it.size)) == prefix })
+                require(binding is Join)
+                val idx = nodeTrace.indexOf(binding.target)
+                check(idx >= 0)
+                val prefix = nodeTrace.subList(0, idx).toSet()
+                if (bindingCounter[binding].any { it.subList(0, min(idx, it.size)).toSet() == prefix })
                     continue
             }
             bindingCounter[binding].add(nodeTrace)
