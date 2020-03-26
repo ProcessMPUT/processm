f778d5ea68770e4dddc2e2377a594087bba3fa0c
Jędrzej Potoniec <jpotoniec@cs.put.poznan.pl>
2020-03-26 09:54:24 +0100

    chore: (main.kotlin.processm.miners.heuristicminer.ReplayTrace) some more doc (#10)

--- processm.miners/src/main/kotlin/processm/miners/heuristicminer/ReplayTrace.kt
+++ processm.miners/src/main/kotlin/processm/miners/heuristicminer/ReplayTrace.kt
@@ -11,11 +11,11 @@ typealias ActiveDependencies = Set<Dependency>
 data class ReplayTrace(
     val state: State,
     /**
-     * Dependencies that were activated in the last step to reach [state]
+     * Dependencies that were activated in the last step to reach [state], such that for each dependency its [target] is [state.a]
      */
     val joins: List<ActiveDependencies>,
     /**
-     * Dependencies that were activated in the last step to reach [state]
+     * Dependencies that were activated in the last step to reach [state], such that for each dependency its [source] is [state.a]
      */
     val splits: List<ActiveDependencies>
 )
