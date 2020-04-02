2e33a44261669d44445b15a9a38a3f9ec5b519e5
Jędrzej Potoniec <jpotoniec@cs.put.poznan.pl>
2020-04-02 15:30:55 +0200

    chore: (processm.miners.heuristicminer.NodeTrace) get rid of unused code (#10)

--- processm.miners/src/main/kotlin/processm/miners/heuristicminer/NodeTrace.kt
+++ processm.miners/src/main/kotlin/processm/miners/heuristicminer/NodeTrace.kt
@@ -1,6 +1,5 @@
 package processm.miners.heuristicminer
 
-import processm.core.helpers.Counter
 import processm.core.log.Event
 import processm.core.log.hierarchical.Trace
 import processm.core.models.causalnet.Node
@@ -24,20 +23,3 @@ class BasicTraceToNodeTrace : TraceToNodeTrace {
             .map { node(it) }
             .toList()
 }
-
-class SplittingTraceToNodeTrace : TraceToNodeTrace {
-    override fun invoke(trace: Trace): NodeTrace {
-        val ctr = Counter<String>()
-        val result = ArrayList<Node>()
-        for (e in trace.events.filter { it.lifecycleTransition == "complete" }) {
-            val name = e.conceptName!!
-            ctr.inc(name)
-            val n = if (ctr[name] >= 2) Node(name, ctr[name].toString())
-            else Node(name)
-            result.add(n)
-        }
-        return result
-    }
-
-}
-
