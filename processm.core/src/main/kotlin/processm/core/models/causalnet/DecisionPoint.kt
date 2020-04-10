53a4c422fb96839eb0f2e96f985899d1d9173ab4
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2020-04-10 11:29:57 +0200

    fix: (processm.core.models.causalnet.DecisionPoint) missing data in class definition (#51)
    PoCDecisionModel uses this
    
    
    Former-commit-id: 191a3dfc52fd64ce30fea8763184cb8d5fd54c9f

--- processm.core/src/main/kotlin/processm/core/models/causalnet/DecisionPoint.kt
+++ processm.core/src/main/kotlin/processm/core/models/causalnet/DecisionPoint.kt
@@ -5,6 +5,6 @@ import processm.core.models.commons.AbstractDecisionPoint
 /**
  * A CausalNet decision point, i.e., choice of a join or a split for the give [node]
  */
-class DecisionPoint(val node: Node, bindings: Set<Binding>) : AbstractDecisionPoint {
+data class DecisionPoint(val node: Node, private val bindings: Set<Binding>) : AbstractDecisionPoint {
     override val possibleOutcomes: List<BindingDecision> = bindings.map { BindingDecision(it, this) }
 }
