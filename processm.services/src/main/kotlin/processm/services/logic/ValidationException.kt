53d06389c5cb5ed44a15a9b44172b15acb360b12
Daniel Sroka <daniel.sroka@put.poznan.pl>
2020-04-17 15:25:26 +0200

    chore: (processm.services.api) add kDoc for custom exceptions (#34)
    Former-commit-id: dde18d2c1edcce67d939c12e2c162e65348cd02d

--- processm.services/src/main/kotlin/processm/services/logic/ValidationException.kt
+++ processm.services/src/main/kotlin/processm/services/logic/ValidationException.kt
@@ -1,5 +1,12 @@
 package processm.services.logic
 
+/**
+ * Represents result of failed validation in business logic.
+ *
+ * @property reason the reason of failed validation.
+ * @property userMessage message explaining details of failed validation.
+ * @property message message passed to parent [Exception]. If not provided, [userMessage] is used.
+ */
 class ValidationException(val reason: Reason, val userMessage: String, message: String? = null) : Exception(message ?: userMessage) {
 
     enum class Reason {
