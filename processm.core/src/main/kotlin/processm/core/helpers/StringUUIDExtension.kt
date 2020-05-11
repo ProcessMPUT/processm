a16c19aac7d017718054c9329e0fba92a5a76a27
Bartosz Górka <bartosz.gorka@interia.eu>
2020-05-11 11:20:58 +0200

    feat: UUID attribute can throw exceptions
    Former-commit-id: 736fad6d1d72f5d56066fdc5c54c0becfe64ad60

--- processm.core/src/main/kotlin/processm/core/helpers/StringUUIDExtension.kt
+++ processm.core/src/main/kotlin/processm/core/helpers/StringUUIDExtension.kt
@@ -1,18 +1,16 @@
 package processm.core.helpers
 
-import processm.core.logging.logger
 import java.util.*
 
 /**
  * Nullable [String] to [UUID]
- * If cast not possible, return `null` value.
  */
 fun String?.toUUID(): UUID? {
     try {
         if (this.isNullOrBlank()) return null
         return UUID.fromString(this)
     } catch (e: IllegalArgumentException) {
-        Helpers.logger().warn("Cast to UUID not possible", e)
+        Helpers.logger.warn("Cast to UUID not possible", e)
     }
 
     return null
--- processm.core/src/test/kotlin/processm/core/log/XESImportCertificationFirstLevelTest.kt
+++ processm.core/src/test/kotlin/processm/core/log/XESImportCertificationFirstLevelTest.kt
@@ -19,29 +19,29 @@ internal class XESImportCertificationFirstLevelTest {
                 <extension name="Concept" prefix="concept" uri="http://www.xes-standard.org/concept.xesext"/>
                 <extension name="Identity" prefix="identity" uri="http://www.xes-standard.org/identity.xesext"/>
                 <string key="concept:name" value="Log concept:name"/>
-                <string key="identity:id" value="bbf3f64f-2507-4f0b-a6f8-0113377d69e4"/>
+                <id key="identity:id" value="bbf3f64f-2507-4f0b-a6f8-0113377d69e4"/>
                 <trace>
                     <string key="concept:name" value="Trace #001"/>
-                    <string key="identity:id" value="ae1a2f41-2d01-479d-b6a3-84f18d790b20"/>
+                    <id key="identity:id" value="ae1a2f41-2d01-479d-b6a3-84f18d790b20"/>
                     <event>
                         <string key="concept:name" value="Event #1 in Trace #001"/>
-                        <string key="identity:id" value="1419fcd5-8fed-4272-8037-453213d8b0d1"/>
+                        <id key="identity:id" value="1419fcd5-8fed-4272-8037-453213d8b0d1"/>
                     </event>
                     <event>
                         <string key="concept:name" value="Event #2 in Trace #001"/>
-                        <string key="identity:id" value="0e461b08-4f5e-4aa2-b0a2-b7779c82b119"/>
+                        <id key="identity:id" value="0e461b08-4f5e-4aa2-b0a2-b7779c82b119"/>
                     </event>
                 </trace>
                 <trace>
                     <string key="concept:name" value="Trace #002"/>
-                    <string key="identity:id" value="a192d6c5-683b-4188-8f73-222227dd4796"/>
+                    <id key="identity:id" value="a192d6c5-683b-4188-8f73-222227dd4796"/>
                     <event>
                         <string key="concept:name" value="Event #1 in Trace #002"/>
-                        <string key="identity:id" value="1a912b4d-6c78-4a4e-8e0f-219fcea53ea7"/>
+                        <id key="identity:id" value="1a912b4d-6c78-4a4e-8e0f-219fcea53ea7"/>
                     </event>
                     <event>
                         <string key="concept:name" value="Event #2 in Trace #002"/>
-                        <string key="identity:id" value="8f14c2ec-83eb-4843-9cb4-c45456a5f3cb"/>
+                        <id key="identity:id" value="8f14c2ec-83eb-4843-9cb4-c45456a5f3cb"/>
                     </event>
                 </trace>
             </log>
@@ -101,29 +101,29 @@ internal class XESImportCertificationFirstLevelTest {
         """<?xml version="1.0" encoding="UTF-8" ?>
             <log xes.version="1.0" xes.features="nested-attributes" openxes.version="1.0RC7" xmlns="http://www.xes-standard.org/">
                 <string key="concept:name" value="Log concept:name"/>
-                <string key="identity:id" value="bbf3f64f-2507-4f0b-a6f8-0113377d69e4"/>
+                <id key="identity:id" value="bbf3f64f-2507-4f0b-a6f8-0113377d69e4"/>
                 <trace>
                     <string key="concept:name" value="Trace #001"/>
-                    <string key="identity:id" value="ae1a2f41-2d01-479d-b6a3-84f18d790b20"/>
+                    <id key="identity:id" value="ae1a2f41-2d01-479d-b6a3-84f18d790b20"/>
                     <event>
                         <string key="concept:name" value="Event #1 in Trace #001"/>
-                        <string key="identity:id" value="1419fcd5-8fed-4272-8037-453213d8b0d1"/>
+                        <id key="identity:id" value="1419fcd5-8fed-4272-8037-453213d8b0d1"/>
                     </event>
                     <event>
                         <string key="concept:name" value="Event #2 in Trace #001"/>
-                        <string key="identity:id" value="0e461b08-4f5e-4aa2-b0a2-b7779c82b119"/>
+                        <id key="identity:id" value="0e461b08-4f5e-4aa2-b0a2-b7779c82b119"/>
                     </event>
                 </trace>
                 <trace>
                     <string key="concept:name" value="Trace #002"/>
-                    <string key="identity:id" value="a192d6c5-683b-4188-8f73-222227dd4796"/>
+                    <id key="identity:id" value="a192d6c5-683b-4188-8f73-222227dd4796"/>
                     <event>
                         <string key="concept:name" value="Event #1 in Trace #002"/>
-                        <string key="identity:id" value="1a912b4d-6c78-4a4e-8e0f-219fcea53ea7"/>
+                        <id key="identity:id" value="1a912b4d-6c78-4a4e-8e0f-219fcea53ea7"/>
                     </event>
                     <event>
                         <string key="concept:name" value="Event #2 in Trace #002"/>
-                        <string key="identity:id" value="8f14c2ec-83eb-4843-9cb4-c45456a5f3cb"/>
+                        <id key="identity:id" value="8f14c2ec-83eb-4843-9cb4-c45456a5f3cb"/>
                     </event>
                 </trace>
             </log>
@@ -181,36 +181,36 @@ internal class XESImportCertificationFirstLevelTest {
                 <extension name="Lifecycle" prefix="lifecycle" uri="http://www.xes-standard.org/lifecycle.xesext"/>
                 <extension name="Time" prefix="time" uri="http://www.xes-standard.org/time.xesext"/>
                 <string key="concept:name" value="Log concept:name"/>
-                <string key="identity:id" value="bbf3f64f-2507-4f0b-a6f8-0113377d69e4"/>
+                <id key="identity:id" value="bbf3f64f-2507-4f0b-a6f8-0113377d69e4"/>
                 <string key="lifecycle:model" value="standard"/>
                 <trace>
                     <string key="concept:name" value="Trace #001"/>
-                    <string key="identity:id" value="ae1a2f41-2d01-479d-b6a3-84f18d790b20"/>
+                    <id key="identity:id" value="ae1a2f41-2d01-479d-b6a3-84f18d790b20"/>
                     <event>
                         <string key="concept:name" value="Event #1 in Trace #001"/>
-                        <string key="identity:id" value="1419fcd5-8fed-4272-8037-453213d8b0d1"/>
+                        <id key="identity:id" value="1419fcd5-8fed-4272-8037-453213d8b0d1"/>
                         <string key="lifecycle:transition" value="start"/>
                         <date key="time:timestamp" value="2005-01-01T00:00:00.000+01:00"/>
                     </event>
                     <event>
                         <string key="concept:name" value="Event #2 in Trace #001"/>
-                        <string key="identity:id" value="0e461b08-4f5e-4aa2-b0a2-b7779c82b119"/>
+                        <id key="identity:id" value="0e461b08-4f5e-4aa2-b0a2-b7779c82b119"/>
                         <string key="lifecycle:transition" value="complete"/>
                         <date key="time:timestamp" value="2005-01-03T00:00:00.000+01:00"/>
                     </event>
                 </trace>
                 <trace>
                     <string key="concept:name" value="Trace #002"/>
-                    <string key="identity:id" value="a192d6c5-683b-4188-8f73-222227dd4796"/>
+                    <id key="identity:id" value="a192d6c5-683b-4188-8f73-222227dd4796"/>
                     <event>
                         <string key="concept:name" value="Event #1 in Trace #002"/>
-                        <string key="identity:id" value="1a912b4d-6c78-4a4e-8e0f-219fcea53ea7"/>
+                        <id key="identity:id" value="1a912b4d-6c78-4a4e-8e0f-219fcea53ea7"/>
                         <string key="lifecycle:transition" value="schedule"/>
                         <date key="time:timestamp" value="2005-01-04T00:00:00.000+01:00"/>
                     </event>
                     <event>
                         <string key="concept:name" value="Event #2 in Trace #002"/>
-                        <string key="identity:id" value="8f14c2ec-83eb-4843-9cb4-c45456a5f3cb"/>
+                        <id key="identity:id" value="8f14c2ec-83eb-4843-9cb4-c45456a5f3cb"/>
                         <string key="lifecycle:transition" value="complete"/>
                         <date key="time:timestamp" value="2005-01-05T00:00:00.000+01:00"/>
                     </event>
@@ -273,14 +273,14 @@ internal class XESImportCertificationFirstLevelTest {
                 <extension name="Time" prefix="time" uri="http://www.xes-standard.org/time.xesext"/>
                 <extension name="Organizational" prefix="org" uri="http://www.xes-standard.org/org.xesext"/>
                 <string key="concept:name" value="Log concept:name"/>
-                <string key="identity:id" value="bbf3f64f-2507-4f0b-a6f8-0113377d69e4"/>
+                <id key="identity:id" value="bbf3f64f-2507-4f0b-a6f8-0113377d69e4"/>
                 <string key="lifecycle:model" value="standard"/>
                 <trace>
                     <string key="concept:name" value="Trace #001"/>
-                    <string key="identity:id" value="ae1a2f41-2d01-479d-b6a3-84f18d790b20"/>
+                    <id key="identity:id" value="ae1a2f41-2d01-479d-b6a3-84f18d790b20"/>
                     <event>
                         <string key="concept:name" value="Event #1 in Trace #001"/>
-                        <string key="identity:id" value="1419fcd5-8fed-4272-8037-453213d8b0d1"/>
+                        <id key="identity:id" value="1419fcd5-8fed-4272-8037-453213d8b0d1"/>
                         <string key="lifecycle:transition" value="start"/>
                         <date key="time:timestamp" value="2005-01-01T00:00:00.000+01:00"/>
                         <string key="org:group" value="Endoscopy"/>
@@ -289,7 +289,7 @@ internal class XESImportCertificationFirstLevelTest {
                     </event>
                     <event>
                         <string key="concept:name" value="Event #2 in Trace #001"/>
-                        <string key="identity:id" value="0e461b08-4f5e-4aa2-b0a2-b7779c82b119"/>
+                        <id key="identity:id" value="0e461b08-4f5e-4aa2-b0a2-b7779c82b119"/>
                         <string key="lifecycle:transition" value="complete"/>
                         <date key="time:timestamp" value="2005-01-03T00:00:00.000+01:00"/>
                         <string key="org:group" value="Endoscopy"/>
@@ -299,10 +299,10 @@ internal class XESImportCertificationFirstLevelTest {
                 </trace>
                 <trace>
                     <string key="concept:name" value="Trace #002"/>
-                    <string key="identity:id" value="a192d6c5-683b-4188-8f73-222227dd4796"/>
+                    <id key="identity:id" value="a192d6c5-683b-4188-8f73-222227dd4796"/>
                     <event>
                         <string key="concept:name" value="Event #1 in Trace #002"/>
-                        <string key="identity:id" value="1a912b4d-6c78-4a4e-8e0f-219fcea53ea7"/>
+                        <id key="identity:id" value="1a912b4d-6c78-4a4e-8e0f-219fcea53ea7"/>
                         <string key="lifecycle:transition" value="schedule"/>
                         <date key="time:timestamp" value="2005-01-04T00:00:00.000+01:00"/>
                         <string key="org:group" value="Radiotherapy"/>
@@ -311,7 +311,7 @@ internal class XESImportCertificationFirstLevelTest {
                     </event>
                     <event>
                         <string key="concept:name" value="Event #2 in Trace #002"/>
-                        <string key="identity:id" value="8f14c2ec-83eb-4843-9cb4-c45456a5f3cb"/>
+                        <id key="identity:id" value="8f14c2ec-83eb-4843-9cb4-c45456a5f3cb"/>
                         <string key="lifecycle:transition" value="complete"/>
                         <date key="time:timestamp" value="2005-01-05T00:00:00.000+01:00"/>
                         <string key="org:group" value="Radiotherapy"/>
@@ -372,16 +372,16 @@ internal class XESImportCertificationFirstLevelTest {
                 <extension name="Organizational" prefix="org" uri="http://www.xes-standard.org/org.xesext"/>
                 <extension name="Cost" prefix="cost" uri="http://www.xes-standard.org/cost.xesext"/>
                 <string key="concept:name" value="Log concept:name"/>
-                <string key="identity:id" value="bbf3f64f-2507-4f0b-a6f8-0113377d69e4"/>
+                <id key="identity:id" value="bbf3f64f-2507-4f0b-a6f8-0113377d69e4"/>
                 <string key="lifecycle:model" value="standard"/>
                 <trace>
                     <string key="concept:name" value="Trace #001"/>
-                    <string key="identity:id" value="ae1a2f41-2d01-479d-b6a3-84f18d790b20"/>
+                    <id key="identity:id" value="ae1a2f41-2d01-479d-b6a3-84f18d790b20"/>
                     <float key="cost:total" value="99.99"/>
                     <string key="cost:currency" value="PLN"/>
                     <event>
                         <string key="concept:name" value="Event #1 in Trace #001"/>
-                        <string key="identity:id" value="1419fcd5-8fed-4272-8037-453213d8b0d1"/>
+                        <id key="identity:id" value="1419fcd5-8fed-4272-8037-453213d8b0d1"/>
                         <string key="lifecycle:transition" value="start"/>
                         <date key="time:timestamp" value="2005-01-01T00:00:00.000+01:00"/>
                         <string key="org:group" value="Endoscopy"/>
@@ -392,7 +392,7 @@ internal class XESImportCertificationFirstLevelTest {
                     </event>
                     <event>
                         <string key="concept:name" value="Event #2 in Trace #001"/>
-                        <string key="identity:id" value="0e461b08-4f5e-4aa2-b0a2-b7779c82b119"/>
+                        <id key="identity:id" value="0e461b08-4f5e-4aa2-b0a2-b7779c82b119"/>
                         <string key="lifecycle:transition" value="complete"/>
                         <date key="time:timestamp" value="2005-01-03T00:00:00.000+01:00"/>
                         <string key="org:group" value="Endoscopy"/>
@@ -404,12 +404,12 @@ internal class XESImportCertificationFirstLevelTest {
                 </trace>
                 <trace>
                     <string key="concept:name" value="Trace #002"/>
-                    <string key="identity:id" value="a192d6c5-683b-4188-8f73-222227dd4796"/>
+                    <id key="identity:id" value="a192d6c5-683b-4188-8f73-222227dd4796"/>
                     <float key="cost:total" value="10.00"/>
                     <string key="cost:currency" value="USD"/>
                     <event>
                         <string key="concept:name" value="Event #1 in Trace #002"/>
-                        <string key="identity:id" value="1a912b4d-6c78-4a4e-8e0f-219fcea53ea7"/>
+                        <id key="identity:id" value="1a912b4d-6c78-4a4e-8e0f-219fcea53ea7"/>
                         <string key="lifecycle:transition" value="schedule"/>
                         <date key="time:timestamp" value="2005-01-04T00:00:00.000+01:00"/>
                         <string key="org:group" value="Radiotherapy"/>
@@ -420,7 +420,7 @@ internal class XESImportCertificationFirstLevelTest {
                     </event>
                     <event>
                         <string key="concept:name" value="Event #2 in Trace #002"/>
-                        <string key="identity:id" value="8f14c2ec-83eb-4843-9cb4-c45456a5f3cb"/>
+                        <id key="identity:id" value="8f14c2ec-83eb-4843-9cb4-c45456a5f3cb"/>
                         <string key="lifecycle:transition" value="complete"/>
                         <date key="time:timestamp" value="2005-01-05T00:00:00.000+01:00"/>
                         <string key="org:group" value="Radiotherapy"/>
@@ -503,17 +503,17 @@ internal class XESImportCertificationFirstLevelTest {
                 <extension name="Cost" prefix="wascost" uri="http://www.xes-standard.org/cost.xesext"/>
                 <extension name="My own extension" prefix="cost" uri="http://example.com/cost.xesext"/>
                 <string key="concept:name" value="Log concept:name"/>
-                <string key="identity:id" value="bbf3f64f-2507-4f0b-a6f8-0113377d69e4"/>
+                <id key="identity:id" value="bbf3f64f-2507-4f0b-a6f8-0113377d69e4"/>
                 <string key="lifecycle:model" value="standard"/>
                 <string key="value-without-extension" value="some-special-value"/>
                 <trace>
                     <string key="concept:name" value="Trace #001"/>
-                    <string key="identity:id" value="ae1a2f41-2d01-479d-b6a3-84f18d790b20"/>
+                    <id key="identity:id" value="ae1a2f41-2d01-479d-b6a3-84f18d790b20"/>
                     <float key="wascost:total" value="99.99"/>
                     <string key="wascost:currency" value="PLN"/>
                     <event>
                         <string key="concept:name" value="Event #1 in Trace #001"/>
-                        <string key="identity:id" value="1419fcd5-8fed-4272-8037-453213d8b0d1"/>
+                        <id key="identity:id" value="1419fcd5-8fed-4272-8037-453213d8b0d1"/>
                         <string key="lifecycle:transition" value="start"/>
                         <date key="time:timestamp" value="2005-01-01T00:00:00.000+01:00"/>
                         <string key="org:group" value="Endoscopy"/>
@@ -525,7 +525,7 @@ internal class XESImportCertificationFirstLevelTest {
                     </event>
                     <event>
                         <string key="concept:name" value="Event #2 in Trace #001"/>
-                        <string key="identity:id" value="0e461b08-4f5e-4aa2-b0a2-b7779c82b119"/>
+                        <id key="identity:id" value="0e461b08-4f5e-4aa2-b0a2-b7779c82b119"/>
                         <string key="lifecycle:transition" value="complete"/>
                         <date key="time:timestamp" value="2005-01-03T00:00:00.000+01:00"/>
                         <string key="org:group" value="Endoscopy"/>
@@ -538,12 +538,12 @@ internal class XESImportCertificationFirstLevelTest {
                 </trace>
                 <trace>
                     <string key="concept:name" value="Trace #002"/>
-                    <string key="identity:id" value="a192d6c5-683b-4188-8f73-222227dd4796"/>
+                    <id key="identity:id" value="a192d6c5-683b-4188-8f73-222227dd4796"/>
                     <float key="wascost:total" value="10.00"/>
                     <string key="wascost:currency" value="USD"/>
                     <event>
                         <string key="concept:name" value="Event #1 in Trace #002"/>
-                        <string key="identity:id" value="1a912b4d-6c78-4a4e-8e0f-219fcea53ea7"/>
+                        <id key="identity:id" value="1a912b4d-6c78-4a4e-8e0f-219fcea53ea7"/>
                         <string key="lifecycle:transition" value="schedule"/>
                         <date key="time:timestamp" value="2005-01-04T00:00:00.000+01:00"/>
                         <string key="org:group" value="Radiotherapy"/>
@@ -555,7 +555,7 @@ internal class XESImportCertificationFirstLevelTest {
                     </event>
                     <event>
                         <string key="concept:name" value="Event #2 in Trace #002"/>
-                        <string key="identity:id" value="8f14c2ec-83eb-4843-9cb4-c45456a5f3cb"/>
+                        <id key="identity:id" value="8f14c2ec-83eb-4843-9cb4-c45456a5f3cb"/>
                         <string key="lifecycle:transition" value="complete"/>
                         <date key="time:timestamp" value="2005-01-05T00:00:00.000+01:00"/>
                         <string key="org:group" value="Radiotherapy"/>
