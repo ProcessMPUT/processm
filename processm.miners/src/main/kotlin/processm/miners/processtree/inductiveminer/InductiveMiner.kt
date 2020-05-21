c0b6c4d9263a1594d145055f49d3566368467d30
Bartosz Górka <bartosz.gorka@interia.eu>
2020-05-21 13:25:12 +0200

    chore: Move common code of IM from Offline version to abstract class
    Former-commit-id: c86984c06b1b7b2f27db50f6a9f951d53518c986

--- processm.miners/src/main/kotlin/processm/miners/processtree/inductiveminer/InductiveMiner.kt
+++ processm.miners/src/main/kotlin/processm/miners/processtree/inductiveminer/InductiveMiner.kt
@@ -1,20 +1,61 @@
 package processm.miners.processtree.inductiveminer
 
 import processm.core.log.hierarchical.Log
-import processm.core.models.processtree.ProcessTree
+import processm.core.models.processtree.*
 
 /**
- * Common interface for Inductive miners.
- *
+ * Inductive miners common abstract implementation.
+ * Can be used by:
+ * - [OfflineInductiveMinerWithoutLogStatistics]
+ * as code-base reduces. You should use [InductiveMiner] instead of duplicating code.
  */
-interface InductiveMiner {
+abstract class InductiveMiner {
     /**
      * Perform mining on a given log.
      */
-    fun processLog(log: Iterable<Log>)
+    abstract fun processLog(log: Iterable<Log>)
 
     /**
      * The mined model
      */
-    val result: ProcessTree
+    abstract val result: ProcessTree
+
+    /**
+     * Internal set of operations when we should analyze children stored in subGraph
+     */
+    private val nestedOperators = setOf(CutType.RedoLoop, CutType.Sequence, CutType.Parallel, CutType.Exclusive)
+
+    /**
+     * Assign children to node discovered by subGraph cut.
+     * Found node can be operator (like exclusive choice) or activity.
+     */
+    internal fun assignChildrenToNode(graph: DirectlyFollowsSubGraph): Node {
+        val node = discoveredCutToNodeObject(graph)
+
+        if (graph.detectedCut in nestedOperators) {
+            val it = graph.children.filterNotNull().iterator()
+            while (it.hasNext()) {
+                with(assignChildrenToNode(it.next())) {
+                    node.addChild(this)
+                }
+            }
+        }
+
+        return node
+    }
+
+    /**
+     * Transform discovered cut in subGraph into node.
+     * If activity or flower-model -> fetch if from graph.
+     */
+    private fun discoveredCutToNodeObject(graph: DirectlyFollowsSubGraph): Node {
+        return when (graph.detectedCut) {
+            CutType.Activity -> graph.finishCalculations()
+            CutType.Exclusive -> Exclusive()
+            CutType.Sequence -> Sequence()
+            CutType.Parallel -> Parallel()
+            CutType.RedoLoop -> RedoLoop()
+            CutType.FlowerModel -> graph.finishWithDefaultRule()
+        }
+    }
 }
