6072645a592e670cee2d74a244ff16614609f520
Bartosz Górka <bartosz.gorka@interia.eu>
2020-08-20 11:58:45 +0200

    feat: Separate migrations
    Former-commit-id: 0d7189233fc666bd58e679d2bb02b7251c9f2d1c

---
+++ processm.core/src/main/resources/db/processm_datastore_migrations/V202001241115__initial_db.sql
@@ -0,0 +1,1 @@
+CREATE EXTENSION IF NOT EXISTS timescaledb SCHEMA ${flyway:defaultSchema} CASCADE;
--- processm.core/src/main/resources/db/processm_datastore_migrations/V202008051215__create_xes_logs_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE logs
-(
-    id                SERIAL PRIMARY KEY,
-    features          TEXT,
-    "concept:name"    TEXT,
-    "identity:id"     TEXT,
-    "lifecycle:model" TEXT
-);
