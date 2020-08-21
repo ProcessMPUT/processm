116a559b5864f54132038a10303a9f8b7ce5eddd
Jędrzej Potoniec <jpotoniec@cs.put.poznan.pl>
2020-08-21 13:46:30 +0200

    fix: (processm.miners.heuristicminer.HashMapWithDefault) using computeIfAbsent (#75)
    Former-commit-id: 2b91a9a341f9bb517ac78d1e3c3e6633e1a1586d

--- processm.miners/src/main/kotlin/processm/miners/heuristicminer/HashMapWithDefault.kt
+++ processm.miners/src/main/kotlin/processm/miners/heuristicminer/HashMapWithDefault.kt
@@ -6,13 +6,5 @@ package processm.miners.heuristicminer
  * Cannot call [getOrPut] directly, as it is an extension function which calls [get]
  */
 class HashMapWithDefault<K, V>(private val default: () -> V) : HashMap<K, V>() {
-    override operator fun get(key: K): V {
-        val result = super.get(key)
-        return if (result == null) {
-            val new = default()
-            this[key] = new
-            new
-        } else
-            result
-    }
+    override operator fun get(key: K): V = super.computeIfAbsent(key) { default() }
 }
