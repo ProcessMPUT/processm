e54cbe2037463c24b0595c4821613a7d06034238
Jedrzej Potoniec <jpotoniec@cs.put.poznan.pl>
2021-02-19 11:20:19 +0100

    feat: (processm.experimental.helpers.BitIntSet) a set for storing ints from 0..n-1 where n is a fixed number
    Former-commit-id: 2fdf7a1ad0949d8c9815033eb1bbedc413af0330

--- processm.experimental/src/test/kotlin/processm/experimental/helpers/BitIntSetTest.kt
+++ processm.experimental/src/test/kotlin/processm/experimental/helpers/BitIntSetTest.kt
@@ -41,4 +41,73 @@ class BitIntSetTest {
         assertEquals(values, recreated)
     }
 
+    @Test
+    fun `addAll size iterate`() {
+        val bitIntSet = BitIntSet(1000)
+        val values = mutableListOf(5, 7, 32, 34, 37, 63, 64, 65, 127, 128, 256, 700)
+        assertEquals(0, bitIntSet.size)
+        bitIntSet.addAll(values)
+        assertEquals(values.size, bitIntSet.size)
+        assertEquals(values, bitIntSet.toList())
+    }
+
+    @Test
+    fun `addAll bitIntSet size iterate`() {
+        val bitIntSet = BitIntSet(1000)
+        val values = mutableListOf(5, 7, 32, 34, 37, 63, 64, 65, 127, 128, 256, 700)
+        assertEquals(0, bitIntSet.size)
+        bitIntSet.addAll(values)
+        assertEquals(values.size, bitIntSet.size)
+        val bitIntSet2 = BitIntSet(1000)
+        bitIntSet2.addAll(bitIntSet)
+        assertEquals(values.size, bitIntSet2.size)
+        assertEquals(values, bitIntSet2.toList())
+    }
+
+    @Test
+    fun `addAll bitIntSet to non empty size iterate`() {
+        val values1 = listOf(5, 7, 32, 34, 37, 63, 64)
+        val bitIntSet = BitIntSet(1000)
+        bitIntSet.addAll(values1)
+        val values2 = listOf(65, 127, 128, 256, 700)
+        val bitIntSet2 = BitIntSet(1000)
+        bitIntSet2.addAll(values2)
+        bitIntSet.addAll(bitIntSet2)
+        val expected=values1+values2
+        assertEquals(expected.size, bitIntSet.size)
+        assertEquals(expected, bitIntSet.toList())
+    }
+
+    @Test
+    fun `addAll removeAll size`() {
+        val bitIntSet = BitIntSet(1000)
+        val values = setOf(5, 7, 32, 34, 37, 63, 64, 65, 127, 128, 256, 700)
+        assertEquals(0, bitIntSet.size)
+        bitIntSet.addAll(values)
+        assertEquals(values.size, bitIntSet.size)
+        val bitIntSet2 = BitIntSet(1000)
+        val values2 = setOf(37, 63, 64, 65, 127, 128, 256, 257, 258, 259, 260)
+        bitIntSet2.addAll(values2)
+        bitIntSet.removeAll(bitIntSet2)
+        val expected = values-values2
+        assertEquals(expected.size, bitIntSet.size )
+        assertEquals(expected, bitIntSet)
+    }
+
+    @Test
+    fun `single element toList`() {
+        val bitIntSet = BitIntSet(1000)
+        bitIntSet.add(5)
+        assertEquals(listOf(5), bitIntSet.toList())
+    }
+
+    @Test
+    fun `single element on the border`() {
+        val bitIntSet = BitIntSet(1000)
+        bitIntSet.add(191)
+        val i =bitIntSet.iterator()
+        assertEquals(191, i.next())
+        assertFalse(i.hasNext())
+    }
+
 }
