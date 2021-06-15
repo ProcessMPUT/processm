0dbf8b901f73e2c6b53a1b7a03099b04513682e7
Jedrzej Potoniec <jpotoniec@cs.put.poznan.pl>
2021-06-15 16:46:55 +0200

    feat: (processm.core.helpers) HashMapWithDefault (#75)
    Former-commit-id: 4cd1cd93a99de9273cda4dc23082d0158b21708a

---
+++ processm.core/src/main/kotlin/processm/core/helpers/HashMapWithDefault.kt
@@ -0,0 +1,10 @@
+package processm.core.helpers
+
+/**
+ * Map with [get] behaving like [getOrPut].
+ *
+ * Cannot call [getOrPut] directly, as it is an extension function which calls [get]
+ */
+class HashMapWithDefault<K, V>(private val default: () -> V) : HashMap<K, V>() {
+    override operator fun get(key: K): V = super.computeIfAbsent(key) { default() }
+}
