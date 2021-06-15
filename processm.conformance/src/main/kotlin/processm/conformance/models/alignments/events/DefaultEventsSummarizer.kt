962dc7f317c97741991178d8262eee6037ea8252
Jedrzej Potoniec <jpotoniec@cs.put.poznan.pl>
2021-06-15 16:16:53 +0200

    chore: (processm.conformance.models.alignments) tests and doc for log alignments (#92)
    Former-commit-id: bb1d8a6b6dde568839a28a56ab2e56c54979d7c9

--- processm.conformance/src/main/kotlin/processm/conformance/models/alignments/events/DefaultEventsSummarizer.kt
+++ processm.conformance/src/main/kotlin/processm/conformance/models/alignments/events/DefaultEventsSummarizer.kt
@@ -2,6 +2,9 @@ package processm.conformance.models.alignments.events
 
 import processm.core.log.Event
 
+/**
+ * An [EventsSummarizer] assuming that every piece of information is important
+ */
 class DefaultEventsSummarizer : EventsSummarizer<List<Event>> {
     override fun invoke(events: List<Event>) = events
 }
