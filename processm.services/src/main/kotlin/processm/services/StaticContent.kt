a31b8bfc1ff06ec3d102eccd2364e11eb9f2afde
Daniel Sroka <daniel.sroka@put.poznan.pl>
2021-06-23 15:07:47 +0200

    feat: (processm.webui) add favicon (#94)
    Former-commit-id: 31211d1d6c15af70f692b0361eb0e297ee68f178

--- processm.services/src/main/kotlin/processm/services/StaticContent.kt
+++ processm.services/src/main/kotlin/processm/services/StaticContent.kt
@@ -5,45 +5,43 @@ import io.ktor.http.content.*
 import io.ktor.routing.routing
 import processm.core.logging.enter
 import processm.core.logging.exit
+import processm.core.logging.loggedScope
 import processm.core.logging.logger
 import java.io.File
 
 fun Application.staticContentModule() {
-    logger().enter()
-    val codeSource = File(WebServicesHost::class.java.protectionDomain.codeSource.location.toURI())
-    val jar = codeSource.extension.equals("jar", true)
+    loggedScope { logger ->
+        val codeSource = File(WebServicesHost::class.java.protectionDomain.codeSource.location.toURI())
+        val jar = codeSource.extension.equals("jar", true)
 
-    routing {
-        static("") {
-            if (jar) {
-                logger().debug("Serving static content from JAR")
-                staticBasePackage = "processm.webui"
-                resources("frontend-dist")
-                defaultResource("frontend-dist/index.html")
-            } else {
-                logger().debug("Serving static content from file system")
-                staticRootFolder =
-                    codeSource.parentFile.parentFile.parentFile.resolve("processm.webui/target/classes/frontend-dist")
-                files(".")
-                default("index.html")
+        routing {
+            static("") {
+                if (jar) {
+                    logger.info("Serving static content from JAR")
+                    resources("frontend-dist")
+                    defaultResource("frontend-dist/index.html")
+                } else {
+                    logger.info("Serving static content from file system")
+                    staticRootFolder =
+                        codeSource.parentFile.parentFile.parentFile.resolve("processm.webui/target/classes/frontend-dist")
+                    files(".")
+                    default("index.html")
+                }
             }
-        }
-        static("api-docs") {
-            if (jar) {
-                logger().debug("Serving static API doc")
-                staticBasePackage = "processm.services"
-                resources("openapi")
-                defaultResource("openapi/index.html")
-            } else {
-                logger().debug("Serving Swagger UI")
-                staticRootFolder =
-                    codeSource.parentFile.parentFile.parentFile.resolve("processm.services/target/swagger-ui")
-                file("api-spec.yaml", codeSource.resolve("api-spec.yaml"))
-                files(".")
-                default("index.html")
+            static("api-docs") {
+                if (jar) {
+                    logger.info("Serving static API doc")
+                    resources("openapi")
+                    defaultResource("openapi/index.html")
+                } else {
+                    logger.info("Serving Swagger UI")
+                    staticRootFolder =
+                        codeSource.parentFile.parentFile.parentFile.resolve("processm.services/target/swagger-ui")
+                    file("api-spec.yaml", codeSource.resolve("api-spec.yaml"))
+                    files(".")
+                    default("index.html")
+                }
             }
         }
     }
-
-    logger().exit()
 }
