19afa00cf66da9ca43ca3bf3344ec58d667e5068
Jedrzej Potoniec <jpotoniec@cs.put.poznan.pl>
2021-07-12 14:45:10 +0200

    processm.etl: Flink PoC using Flink features
    Former-commit-id: 40b848dc161e9c9e6cb7cfffc50a241dde275b23

---
+++ processm.etl/src/main/kotlin/processm/etl/ArtemisScanTableSource.kt
@@ -0,0 +1,35 @@
+package processm.etl
+
+import org.apache.flink.api.common.serialization.DeserializationSchema
+import org.apache.flink.table.connector.ChangelogMode
+import org.apache.flink.table.connector.format.DecodingFormat
+import org.apache.flink.table.connector.source.DynamicTableSource
+import org.apache.flink.table.connector.source.ScanTableSource
+import org.apache.flink.table.connector.source.SourceFunctionProvider
+import org.apache.flink.table.data.RowData
+import org.apache.flink.table.types.DataType
+
+
+class ArtemisScanTableSource(
+    val topic: String,
+    val decodingFormat: DecodingFormat<DeserializationSchema<RowData>>,
+    val producedDataType: DataType
+) : ScanTableSource {
+
+    override fun copy(): DynamicTableSource {
+        TODO("Not yet implemented")
+    }
+
+    override fun asSummaryString(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getChangelogMode(): ChangelogMode = ChangelogMode.insertOnly()
+
+    override fun getScanRuntimeProvider(runtimeProviderContext: ScanTableSource.ScanContext?): ScanTableSource.ScanRuntimeProvider {
+        val decoder = decodingFormat.createRuntimeDecoder(runtimeProviderContext, producedDataType)
+
+        val sourceFunction = ArtemisSourceFunction(topic, decoder)
+        return SourceFunctionProvider.of(sourceFunction, false)
+    }
+}
