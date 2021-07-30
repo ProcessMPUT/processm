a2c383840c3ea7ae4e2ff7feede592a876e22d89
tpawlak <9dhNTagE5NAN1Gv_XJ7o>
2021-07-30 16:14:12 +0200

    feat: add the global snackbar for info/warning/error messages (#99)
    Former-commit-id: 490e5469fbc4cea1e29f27a36f0f4f23e8237062

--- processm.webui/src/main/frontend-app/src/App.vue
+++ processm.webui/src/main/frontend-app/src/App.vue
@@ -7,6 +7,19 @@
     <v-main>
       <router-view />
     </v-main>
+
+    <v-snackbar
+      :color="snack.color"
+      v-model="snack.visible"
+      :timeout="snack.timeout"
+    >
+      {{ snack.message }}
+      <template v-slot:action="{ attrs }">
+        <v-btn text v-bind="attrs" @click="snack.visible = false">
+          {{ $t("common.close") }}
+        </v-btn>
+      </template>
+    </v-snackbar>
   </v-app>
 </template>
 
@@ -15,9 +28,38 @@ import Vue from "vue";
 import Component from "vue-class-component";
 import AppNavigation from "@/components/AppNavigation.vue";
 import TopBar from "@/components/TopBar.vue";
+import { Provide } from "vue-property-decorator";
 
 @Component({
   components: { AppNavigation, TopBar }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  @Provide("app") app = this;
+  snack = {
+    color: "info",
+    message: "",
+    visible: false,
+    timeout: 10000
+  };
+
+  private message(color: string, text: string, timeout = 10000) {
+    if (text == "") return;
+    this.snack.color = color;
+    this.snack.message = text;
+    this.snack.timeout = timeout;
+    this.snack.visible = true;
+  }
+
+  info(text: string, timeout = 10000) {
+    this.message("info", text, timeout);
+  }
+
+  warn(text: string, timeout = 10000) {
+    this.message("warning", text, timeout);
+  }
+
+  error(text: string, timeout = 10000) {
+    this.message("error", text, timeout);
+  }
+}
 </script>
