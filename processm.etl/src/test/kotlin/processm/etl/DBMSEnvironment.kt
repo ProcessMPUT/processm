da61e1cf99a0b12d3b1e87acb26ab8c3e34e3166
tpawlak <9dhNTagE5NAN1Gv_XJ7o>
2021-08-20 14:22:14 +0200

    feat: add the test helper for creating dockerized test databases (#110)
    Former-commit-id: a67c63e6dc192eb824a286f46b304056ed34eacd

---
+++ processm.etl/src/test/kotlin/processm/etl/DBMSEnvironment.kt
@@ -0,0 +1,48 @@
+package processm.etl
+
+import org.testcontainers.containers.JdbcDatabaseContainer
+import org.testcontainers.lifecycle.Startables
+import java.sql.Connection
+
+/**
+ * The base class for simulating external databases. The derived classes implement the specifics of concrete database
+ * management systems.
+ */
+abstract class DBMSEnvironment<Container : JdbcDatabaseContainer<*>>(
+    val dbName: String,
+    val user: String,
+    val password: String,
+    val schemaScript: String,
+    val insertScript: String
+) : AutoCloseable {
+    private val container: Container = initAndRun()
+
+    protected abstract fun initContainer(): Container
+
+    private fun initAndRun(): Container {
+        val container = initContainer()
+            .withDatabaseName(dbName)
+            .withUsername(user)
+            .withPassword(password)
+            .withInitScript(schemaScript)
+        Startables.deepStart(listOf(container)).join()
+
+        container.createConnection("").use { connection ->
+            connection.autoCommit = false
+            connection.createStatement().use { s ->
+                s.execute(this::class.java.classLoader.getResource(insertScript)!!.readText())
+            }
+            connection.commit()
+        }
+
+        return container as Container
+    }
+
+    val jdbcUrl: String = container.jdbcUrl
+
+    fun connect(): Connection = container.createConnection("")
+
+    override fun close() {
+        container.close()
+    }
+}
