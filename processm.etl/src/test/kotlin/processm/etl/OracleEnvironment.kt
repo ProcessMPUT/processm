deae1b06a32678d29c23b9d7da104a148e43ab66
Jedrzej Potoniec <jpotoniec@cs.put.poznan.pl>
2021-09-23 15:27:36 +0200

    chore: (processm.etl.jdbc.OracleOTSampleDbTest) a simple tests based on the Oracle Sample Database (#111)
    Former-commit-id: e7e83eb3b6bd2422aac2a961d94021a47e84ee05

--- processm.etl/src/test/kotlin/processm/etl/OracleEnvironment.kt
+++ processm.etl/src/test/kotlin/processm/etl/OracleEnvironment.kt
@@ -5,6 +5,83 @@ import org.testcontainers.lifecycle.Startables
 import org.testcontainers.utility.DockerImageName
 import org.testcontainers.utility.MountableFile
 import processm.core.logging.logger
+import java.io.File
+
+
+/**
+ * An [OracleContainer] with support for SIDs
+ */
+class MyOracleContainer(dockerImageName: DockerImageName) : OracleContainer(dockerImageName) {
+
+    private var _sid: String = "xe"
+
+    fun withSid(sid: String) {
+        _sid = sid
+    }
+
+    override fun getSid(): String = _sid
+
+    override fun getJdbcUrl(): String = "jdbc:oracle:thin:$username/$password@$host:$oraclePort/$sid"
+}
+
+class OracleScriptConfigurator(private vararg val scripts: String) :
+    DBMSEnvironmentConfigurator<OracleEnvironment, MyOracleContainer> {
+    private val scriptsInContainer = ArrayList<String>()
+    override fun beforeStart(environment: OracleEnvironment, container: MyOracleContainer) {
+        for ((idx, script) in scripts.withIndex()) {
+            val path = "/tmp/$idx.sql"
+            container.withCopyFileToContainer(MountableFile.forClasspathResource(script), path)
+            scriptsInContainer.add(path)
+        }
+    }
+
+    override fun afterStart(environment: OracleEnvironment, container: MyOracleContainer) {
+        for (script in scriptsInContainer) {
+            container.execInContainer(
+                "sqlplus",
+                "${container.username}/${container.password}@localhost:1521/${container.sid}",
+                "@$script"
+            )
+        }
+    }
+}
+
+class OracleSampleConfigurator : DBMSEnvironmentConfigurator<OracleEnvironment, MyOracleContainer> {
+    private val scriptPath = "/tmp/script.sh"
+
+    override fun beforeStart(environment: OracleEnvironment, container: MyOracleContainer) {
+        container.withCopyFileToContainer(
+            MountableFile.forClasspathResource("oracle/db-sample-schemas-18c.tar.gz"),
+            "/tmp/sample.tar.gz"
+        )
+        val f = File.createTempFile("processm", null)
+        f.deleteOnExit()
+        val connectString = "localhost:1521/xepdb1"
+        val sqlplus = "sqlplus 'SYS/${container.password}@$connectString'  AS SYSDBA"
+        f.writeText(
+            """
+#!/bin/sh
+cd /opt/oracle/product/18c/dbhomeXE/md/admin/
+# The following two lines enable Oracle Spatial, necessary for the OE schema
+$sqlplus '@mdprivs.sql'
+$sqlplus '@mdinst.sql'
+cd /tmp
+tar xf sample.tar.gz
+cd db-sample-schemas-18c
+ln -s . __SUB__CWD__
+$sqlplus '@mksample.sql' '${container.password}' '${container.password}' hrpw oepw pmpw ixpw shpw bipw example temp /tmp/logs '$connectString'
+            """.trimIndent()
+        )
+        container.withCopyFileToContainer(MountableFile.forHostPath(f.absolutePath), scriptPath)
+    }
+
+    override fun afterStart(environment: OracleEnvironment, container: MyOracleContainer) {
+        container.execInContainer("sh", scriptPath)
+        container.withSid("xepdb1")
+    }
+
+}
+
 
 /**
  * A test environment with Oracle Express.
@@ -18,9 +95,8 @@ import processm.core.logging.logger
  * Grouping multiple inserts in a similar way as in [Db2Environment] doesn't seem to help, to the point of being actually slower.
  */
 class OracleEnvironment(
-    val schemaScript: String,
-    val insertScript: String
-) : DBMSEnvironment<OracleContainer>(
+    val configurator: DBMSEnvironmentConfigurator<OracleEnvironment, MyOracleContainer>
+) : DBMSEnvironment<MyOracleContainer>(
     "",
     DEFAULT_USER,
     DEFAULT_PASSWORD
@@ -35,38 +111,29 @@ class OracleEnvironment(
 
         fun getSakila(): OracleEnvironment =
             OracleEnvironment(
-                "sakila/oracle-sakila-db/oracle-sakila-schema.sql",
-                "sakila/oracle-sakila-db/oracle-sakila-insert-data.sql"
+                OracleScriptConfigurator(
+                    "sakila/oracle-sakila-db/oracle-sakila-schema.sql",
+                    "sakila/oracle-sakila-db/oracle-sakila-insert-data.sql"
+                )
             )
+
+        fun getOTSampleDb(): OracleEnvironment = OracleEnvironment(OracleSampleConfigurator())
     }
 
-    override fun initAndRun(): OracleContainer {
+    override fun initAndRun(): MyOracleContainer {
         val container = initContainer()
-            .withCopyFileToContainer(MountableFile.forClasspathResource(schemaScript), "/schema.sql")
-            .withCopyFileToContainer(MountableFile.forClasspathResource(insertScript), "/insert.sql")
-
+        configurator.beforeStart(this, container)
         Startables.deepStart(listOf(container)).join()
-
-        container.execInContainer(
-            "sqlplus",
-            "${container.username}/${container.password}@localhost:1521/${container.sid}",
-            "@/schema.sql"
-        )
-
-        container.execInContainer(
-            "sqlplus",
-            "${container.username}/${container.password}@localhost:1521/${container.sid}",
-            "@/insert.sql"
-        )
-
-        return container as OracleContainer
+        configurator.afterStart(this, container)
+        return container
     }
 
-    override fun initContainer(): OracleContainer {
+    override fun initContainer(): MyOracleContainer {
         val imageName = DockerImageName
             .parse("processm/oracle:latest")
             .asCompatibleSubstituteFor("container-registry.oracle.com/database/express:18.4.0-xe")
-        return OracleContainer(imageName)
+        val container = MyOracleContainer(imageName)
+        container
             .withUsername(user)
             .withPassword(password)
             .withStartupTimeoutSeconds(500)
@@ -74,5 +141,6 @@ class OracleEnvironment(
             .withLogConsumer { frame ->
                 logger.info(frame?.utf8String?.trim())
             }
+        return container
     }
 }
---
+++ processm.etl/src/test/kotlin/processm/etl/jdbc/OracleOTSampleDbTest.kt
@@ -0,0 +1,232 @@
+package processm.etl.jdbc
+
+import org.jetbrains.exposed.sql.deleteAll
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.TestInstance
+import processm.core.log.*
+import processm.core.log.attribute.value
+import processm.core.log.hierarchical.DBHierarchicalXESInputStream
+import processm.core.logging.logger
+import processm.core.persistence.connection.DBCache
+import processm.core.querylanguage.Query
+import processm.dbmodels.etl.jdbc.ETLColumnToAttributeMap
+import processm.dbmodels.etl.jdbc.ETLConfiguration
+import processm.dbmodels.etl.jdbc.ETLConfigurations
+import processm.etl.OracleEnvironment
+import java.util.*
+import kotlin.test.AfterTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class OracleOTSampleDbTest {
+
+    private lateinit var externalDB: OracleEnvironment
+
+    private val getEventSQL = """
+        SELECT * FROM (
+            SELECT "trace_id", "concept:name", "concept:instance", "time:timestamp", ROW_NUMBER() OVER (ORDER BY "time:timestamp") AS "event_id" FROM (
+                    SELECT e.EMPLOYEE_ID AS "trace_id", 'hire' AS "concept:name", NULL AS "concept:instance", e.HIRE_DATE AS "time:timestamp" FROM hr.EMPLOYEES e 
+                UNION ALL
+                    SELECT jh.EMPLOYEE_ID AS "trace_id", 'change_position' AS "concept:name", NULL AS "concept:instance", jh.END_DATE AS "time:timestamp" FROM hr.JOB_HISTORY jh
+                UNION ALL
+                    SELECT o.SALES_REP_ID AS "trace_id", 'handle_order' AS "concept:name", o.ORDER_ID AS "concept:instance", o.ORDER_DATE AS "time:timestamp" FROM oe.ORDERS o WHERE o.SALES_REP_ID IS NOT NULL 
+            ) sub
+        ) sub2
+        WHERE "event_id" > CAST(? AS NUMBER(*, 0))
+    """.trimIndent()
+
+    @BeforeAll
+    fun setUp() {
+        externalDB = OracleEnvironment.getOTSampleDb()
+    }
+
+    @AfterAll
+    fun tearDown() {
+        externalDB.close()
+        DBCache.get(dataStoreName).close()
+        DBCache.getMainDBPool().getConnection().use { conn ->
+            conn.prepareStatement("""DROP DATABASE "$dataStoreName"""").execute()
+        }
+    }
+
+    @AfterTest
+    fun resetState() {
+        DBCache.get(dataStoreName).getConnection().use { conn ->
+            conn.autoCommit = false
+
+            conn.createStatement().use { stmt ->
+                stmt.execute("UPDATE etl_configurations SET last_event_external_id=0")
+                stmt.executeQuery("""SELECT l.id FROM logs l JOIN etl_configurations e ON l."identity:id"=e.log_identity_id""")
+                    .use {
+                        while (it.next())
+                            DBLogCleaner.removeLog(conn, it.getInt(1))
+                    }
+            }
+
+            conn.commit()
+        }
+        transaction(DBCache.get(dataStoreName).database) {
+            ETLConfigurations.deleteAll()
+        }
+    }
+
+    private val logger = logger()
+    private val dataStoreName = UUID.randomUUID().toString()
+    private val etlConfiguratioName = "Oracle Sample DB ETL Test"
+
+    private fun createEtlConfiguration(lastEventExternalId: String? = "0") {
+        transaction(DBCache.get(dataStoreName).database) {
+            val config = ETLConfiguration.new {
+                name = etlConfiguratioName
+                jdbcUri = externalDB.jdbcUrl
+                user = externalDB.user
+                password = externalDB.password
+                query = getEventSQL
+                this.lastEventExternalId = lastEventExternalId
+            }
+
+            ETLColumnToAttributeMap.new {
+                configuration = config
+                sourceColumn = "event_id"
+                target = "event_id"
+                eventId = true
+            }
+
+            ETLColumnToAttributeMap.new {
+                configuration = config
+                sourceColumn = "trace_id"
+                target = "trace_id"
+                traceId = true
+            }
+        }
+    }
+
+    private val expectedNumberOfTraces = 107
+    private val expectedNumberOfEvents = 187
+
+    @Test
+    fun `read XES from existing data`() {
+        createEtlConfiguration()
+        transaction(DBCache.get(dataStoreName).database) {
+            val etl = ETLConfiguration.find { ETLConfigurations.name eq etlConfiguratioName }.first()
+            val stream = etl.toXESInputStream()
+            val list = stream.toList()
+
+            assertEquals(1, list.count { it is Log })
+            assertEquals(expectedNumberOfTraces, list.filterIsInstance<Trace>().groupBy { it.identityId }.count())
+            assertEquals(expectedNumberOfEvents, list.count { it is Event })
+
+            for (event in list.filterIsInstance<Event>()) {
+                assertTrue(event.conceptName in setOf("hire", "change_position", "handle_order"))
+                assertTrue(event.identityId!!.mostSignificantBits == 0L)
+                assertTrue(event.identityId!!.leastSignificantBits <= Int.MAX_VALUE)
+            }
+
+            assertEquals(expectedNumberOfEvents.toDouble().toString(), etl.lastEventExternalId)
+        }
+    }
+
+    @Test
+    fun `read XES from existing data and write it to data store`() {
+        createEtlConfiguration()
+        var logUUID: UUID? = null
+        logger.info("Importing XES...")
+        transaction(DBCache.get(dataStoreName).database) {
+            val etl = ETLConfiguration.find { ETLConfigurations.name eq etlConfiguratioName }.first()
+            AppendingDBXESOutputStream(DBCache.get(dataStoreName).getConnection()).use { out ->
+                out.write(etl.toXESInputStream())
+            }
+            logUUID = etl.logIdentityId
+        }
+
+        logger.info("Querying...")
+        val counts = DBHierarchicalXESInputStream(
+            dataStoreName,
+            Query("select count(l:id), count(t:id), count(e:id) where l:id=$logUUID")
+        )
+
+        val log = counts.first()
+        assertEquals(1L, log.attributes["count(log:identity:id)"]?.value)
+        assertEquals(expectedNumberOfTraces.toLong(), log.traces.first().attributes["count(trace:identity:id)"]?.value)
+        assertEquals(
+            expectedNumberOfEvents.toLong(),
+            log.traces.first().events.first().attributes["count(event:identity:id)"]?.value
+        )
+
+        logger.info("Verifying contents...")
+        val invalidContent =
+            DBHierarchicalXESInputStream(
+                dataStoreName,
+                Query("where l:id=$logUUID and e:name not in ('hire', 'change_position', 'handle_order')")
+            )
+        assertEquals(0, invalidContent.count())
+    }
+
+    @Test
+    fun `read partially XES from existing data and write it to data store then read the remaining XES and write it to data store`() {
+        createEtlConfiguration()
+        val partSize = 100
+        var logUUID: UUID? = null
+        logger.info("Importing the first $partSize XES components...")
+        transaction(DBCache.get(dataStoreName).database) {
+            val etl = ETLConfiguration.find { ETLConfigurations.name eq etlConfiguratioName }.first()
+            AppendingDBXESOutputStream(DBCache.get(dataStoreName).getConnection()).use { out ->
+                val materialized = etl.toXESInputStream().take(partSize).toList()
+                out.write(materialized.asSequence())
+            }
+
+            assertEquals("49.0", etl.lastEventExternalId)
+            logUUID = etl.logIdentityId
+        }
+
+        logger.info("Querying...")
+        var counts = DBHierarchicalXESInputStream(
+            dataStoreName,
+            Query("select count(l:id), count(t:id), count(e:id) where l:id=$logUUID")
+        )
+
+        var log = counts.first()
+        assertEquals(1L, log.attributes["count(log:identity:id)"]?.value)
+        assertEquals(47L, log.traces.first().attributes["count(trace:identity:id)"]?.value)
+        assertEquals(49L, log.traces.first().events.first().attributes["count(event:identity:id)"]?.value)
+
+        // import the remaining components
+        logger.info("Importing the remaining XES components...")
+        transaction(DBCache.get(dataStoreName).database) {
+            val etl = ETLConfiguration.find { ETLConfigurations.name eq etlConfiguratioName }.first()
+            AppendingDBXESOutputStream(DBCache.get(dataStoreName).getConnection()).use { out ->
+                out.write(etl.toXESInputStream())
+            }
+            assertEquals(expectedNumberOfEvents.toDouble().toString(), etl.lastEventExternalId)
+        }
+
+        logger.info("Querying...")
+        counts = DBHierarchicalXESInputStream(
+            dataStoreName,
+            Query("select count(l:id), count(t:id), count(e:id) where l:id=$logUUID")
+        )
+        log = counts.first()
+        assertEquals(1L, log.attributes["count(log:identity:id)"]?.value)
+        assertEquals(expectedNumberOfTraces.toLong(), log.traces.first().attributes["count(trace:identity:id)"]?.value)
+        assertEquals(
+            expectedNumberOfEvents.toLong(),
+            log.traces.first().events.first().attributes["count(event:identity:id)"]?.value
+        )
+    }
+
+    @Test
+    fun `read XES from existing data starting from null`() {
+        createEtlConfiguration(null)
+        transaction(DBCache.get(dataStoreName).database) {
+            val etl = ETLConfiguration.find { ETLConfigurations.name eq etlConfiguratioName }.first()
+            val stream = etl.toXESInputStream()
+            val list = stream.toList()
+
+            assertTrue {list.isEmpty()}
+        }
+    }
+}
