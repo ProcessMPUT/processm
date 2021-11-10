2a6227b7a457ac518f2c24954715a1d5e7deb0e7
Daniel Sroka <daniel.sroka@put.poznan.pl>
2021-11-10 16:47:39 +0100

    feat: adapt meta model to be used in processm.services (#129)
    Former-commit-id: 69eb04b71fccc77e5990ec43ee2a751a54fabd71

--- processm.etl/src/main/kotlin/processm/etl/discovery/DatabaseExplorer.kt
+++ processm.etl/src/main/kotlin/processm/etl/discovery/DatabaseExplorer.kt
@@ -2,11 +2,11 @@ package processm.etl.discovery
 
 import java.io.Closeable
 
-internal interface DatabaseExplorer : Closeable {
+interface DatabaseExplorer : Closeable {
     fun getClasses(): Set<Class>
     fun getRelationships(): Set<Relationship>
 }
 
-internal data class Attribute(val name: String, val type: String, val isPartOfForeignKey: Boolean)
-internal data class Class(val name: String, val attributes: List<Attribute>)
-internal data class Relationship(val name: String, val sourceClass: Class, val targetClass: Class, val sourceColumnName: String)
+data class Attribute(val name: String, val type: String, val isPartOfForeignKey: Boolean)
+data class Class(val name: String, val attributes: List<Attribute>)
+data class Relationship(val name: String, val sourceClass: Class, val targetClass: Class, val sourceColumnName: String)
