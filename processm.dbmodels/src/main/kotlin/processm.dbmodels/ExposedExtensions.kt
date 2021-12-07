0650ade594b66f1bd1cf26e28d202d50c2d015d0
tpawlak <9dhNTagE5NAN1Gv_XJ7o>
2021-12-07 15:19:50 +0100

    feat: a helper for post-commit hook for kotlin/exposed transactions
    Former-commit-id: 12616584013a75705d35df4e9276bc25a197f43a

--- processm.dbmodels/src/main/kotlin/processm.dbmodels/ExposedExtensions.kt
+++ processm.dbmodels/src/main/kotlin/processm.dbmodels/ExposedExtensions.kt
@@ -1,9 +1,23 @@
 package processm.dbmodels
 
+import org.jetbrains.exposed.dao.Entity
 import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.statements.StatementInterceptor
+import org.jetbrains.exposed.sql.transactions.TransactionManager
 
 internal class
 ILikeOp(expr1: Expression<*>, expr2: Expression<*>) : ComparisonOp(expr1, expr2, "ILIKE")
 
 infix fun <T : String?> ExpressionWithColumnType<T>.ilike(pattern: String): Op<Boolean> =
     ILikeOp(this, QueryParameter(pattern, columnType))
+
+/**
+ * Executes [action] on the given entity just after it was committed. [action] does not run on rollback.
+ */
+fun <T : Comparable<T>> Entity<T>.afterCommit(action: Entity<T>.() -> Unit) {
+    TransactionManager.current().registerInterceptor(object : StatementInterceptor {
+        override fun afterCommit() {
+            action()
+        }
+    })
+}
