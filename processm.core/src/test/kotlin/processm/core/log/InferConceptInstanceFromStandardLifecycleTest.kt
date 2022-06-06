9feb3582294ca5db5889e8a598bd12e2cfb05d7f
tpawlak <9dhNTagE5NAN1Gv_XJ7o>
2022-06-06 16:43:52 +0200

    fix: InferConceptInstanceFromStandardLifecycle loses some attributes in the modified events
    The loss is caused by the property initializers of the inherited properties from XESComponent in EventOrTraceBase that are called after the constructor of XESComponent. The constructor of XESComponent fills the standard XES properties from the map given as the argument, and the initializers then override those values. The workaround relies on calling Event.setStandardAttributes() with the name mapping read from log extensions.
    
    
    Former-commit-id: 70d261e05ed200c1b74081f6d42a3d49b178421c

--- processm.core/src/test/kotlin/processm/core/log/InferConceptInstanceFromStandardLifecycleTest.kt
+++ processm.core/src/test/kotlin/processm/core/log/InferConceptInstanceFromStandardLifecycleTest.kt
@@ -47,6 +47,46 @@ class InferConceptInstanceFromStandardLifecycleTest {
     }
 
     @Test
+    fun `verify whether other attributes are not affected`() {
+        val original = this::class.java.getResourceAsStream("/xes-logs/JournalReview-extra.xes").use { stream ->
+            XMLXESInputStream(stream).toList()
+        }
+        this::class.java.getResourceAsStream("/xes-logs/JournalReview-extra.xes").use { stream ->
+            val xes = InferConceptInstanceFromStandardLifecycle(XMLXESInputStream(stream))
+
+            for ((i, component) in xes.withIndex()) {
+                val orig = original[i]
+                assertEquals(orig.javaClass, component.javaClass)
+                assertEquals(orig.conceptName, component.conceptName)
+                assertEquals(orig.identityId, component.identityId)
+                when (component) {
+                    is Log -> {
+                        orig as Log
+                        assertEquals(orig.xesVersion, component.xesVersion)
+                        assertEquals(orig.xesFeatures, component.xesFeatures)
+                        assertEquals(orig.lifecycleModel, component.lifecycleModel)
+                        assertEquals(orig.eventClassifiers, component.eventClassifiers)
+                        assertEquals(orig.traceClassifiers, component.traceClassifiers)
+                        assertEquals(orig.eventGlobals, component.eventGlobals)
+                        assertEquals(orig.traceGlobals, component.traceGlobals)
+                        assertEquals(orig.extensions, component.extensions)
+                    }
+                    is Event -> {
+                        orig as Event
+                        assertEquals(orig.costCurrency, component.costCurrency)
+                        assertEquals(orig.costTotal, component.costTotal)
+                        assertEquals(orig.lifecycleState, component.lifecycleState)
+                        assertEquals(orig.timeTimestamp, component.timeTimestamp)
+                        assertEquals(orig.orgGroup, component.orgGroup)
+                        assertEquals(orig.orgResource, component.orgResource)
+                        assertEquals(orig.orgRole, component.orgRole)
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
     @Ignore
     fun `print XES with inferred concept instance`() {
         val xmlFactory = XMLOutputFactory.newInstance()
