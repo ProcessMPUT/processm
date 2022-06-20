db726615f71fc772a9c53951051cd52dc2717e38
Daniel Sroka <daniel.sroka@put.poznan.pl>
2022-06-20 17:18:06 +0200

    fix: PetriNetTest: check previous activities (#136)
    Former-commit-id: efa67cd9ae0d8ce3f52f1afde2208e06fc34b44a

--- processm.core/src/main/kotlin/processm/core/models/petrinet/PetriNet.kt
+++ processm.core/src/main/kotlin/processm/core/models/petrinet/PetriNet.kt
@@ -49,7 +49,7 @@ class PetriNet(
 
             if (res.size > 1) {
                 val commonPlaces = res.flatMapTo(HashSet()) { t -> t.inPlaces }
-                val previousActivities = transitions.filter { t -> t.outPlaces.any { commonPlaces.contains(it) } }
+                val previousActivities = transitions.filterTo(HashSet()) { t -> t.outPlaces.any { commonPlaces.contains(it) } }
                 yield(DecisionPoint(commonPlaces, res, previousActivities))
                 forbidden.add(res)
                 res = null
