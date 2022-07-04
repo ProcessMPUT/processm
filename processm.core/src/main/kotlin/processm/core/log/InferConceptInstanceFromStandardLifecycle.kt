00105ec2b70aa8a6ec5ce8bfa551f550f9067b3a
tpawlak <9dhNTagE5NAN1Gv_XJ7o>
2022-07-04 17:17:44 +0200

    chore: extract the standard lifecycle model to a global object
    Former-commit-id: 62219e1dd84db31283748d1308e71343248ae036

--- processm.core/src/main/kotlin/processm/core/log/InferConceptInstanceFromStandardLifecycle.kt
+++ processm.core/src/main/kotlin/processm/core/log/InferConceptInstanceFromStandardLifecycle.kt
@@ -1,12 +1,13 @@
 package processm.core.log
 
+import processm.core.helpers.HashMapWithDefault
 import processm.core.helpers.map2d.DoublingMap2D
+import processm.core.log.attribute.Attribute.Companion.CONCEPT_INSTANCE
 import processm.core.log.attribute.StringAttr
 import processm.core.models.commons.Activity
 import processm.core.models.petrinet.Marking
 import processm.core.models.petrinet.PetriNetInstance
 import processm.core.models.petrinet.Transition
-import processm.core.models.petrinet.petrinet
 import java.util.*
 
 /**
@@ -17,34 +18,13 @@ import java.util.*
  */
 class InferConceptInstanceFromStandardLifecycle(val base: XESInputStream) : XESInputStream {
     companion object {
-        private const val CONCEPT_INSTANCE = "concept:instance"
-
-        /**
-         * The "standard" lifecycle model from IEEE-1849-2016 encoded as a Petri net.
-         * See IEEE1849-2016 standard Section 7.2 / Figure 5.
-         */
-        private val standardLifecycle = petrinet {
-            P tout "schedule" * "autoskip" // start
-            P tin "schedule" tout "assign" * "_withdraw1" * "_manualskip1" * "_pi_abort1"// ready
-            P tin "assign" * "reassign" tout "reassign" * "start" * "_withdraw2" * "_manualskip2" * "_pi_abort2" // assigned
-            P tin "start" * "resume" tout "complete" * "suspend" * "_pi_abort3" * "_ate_abort1" // in progress
-            P tin "suspend" tout "resume" * "_pi_abort4" * "_ate_abort2" // suspended
-            P tin "_withdraw1" * "_withdraw2" tout "withdraw"
-            P tin "_manualskip1" * "_manualskip2" tout "manualskip"
-            P tin "_pi_abort1" * "_pi_abort2" * "_pi_abort3" * "_pi_abort4" tout "pi_abort"
-            P tin "_ate_abort1" * "_ate_abort2" tout "ate_abort"
-            P tin "autoskip" * "withdraw" * "manualskip" * "pi_abort" * "ate_abort" * "complete" // end
-        }
-
-        private val transitions = standardLifecycle.transitions.filter { !it.isSilent }.associateBy { it.name }
+        private val transitions = StandardLifecycle.transitions.filter { !it.isSilent }.associateBy { it.name }
     }
 
     override fun iterator(): Iterator<XESComponent> = sequence<XESComponent> {
         // key1: activity name, key2: concept:instance, value: the current state of the lifecycle model
         val activityInstanceToLifecycle = DoublingMap2D<String, String, PetriNetInstance>()
-        val nameMap = object : HashMap<String, String>() {
-            override fun get(key: String): String = super.get(key) ?: key
-        }
+        val nameMap = HashMapWithDefault<String, String>(false) { k -> k }
         for (component in base) {
             when (component) {
                 is Log -> {
@@ -73,7 +53,7 @@ class InferConceptInstanceFromStandardLifecycle(val base: XESInputStream) : XESI
                         ) { _, _, old ->
                             val marking =
                                 Marking(transitions[component.lifecycleTransition!!]!!.outPlaces.associateWith { 1 })
-                            (old ?: standardLifecycle.createInstance()).apply { setState(marking) }
+                            (old ?: StandardLifecycle.createInstance()).apply { setState(marking) }
                         }
                         yield(component)
                     } else {
@@ -83,7 +63,7 @@ class InferConceptInstanceFromStandardLifecycle(val base: XESInputStream) : XESI
                             val lifecycleModel = activityInstanceToLifecycle.compute(
                                 component.conceptName!!,
                                 conceptInstance.toString()
-                            ) { _, _, old -> (old ?: standardLifecycle.createInstance()) }!!
+                            ) { _, _, old -> (old ?: StandardLifecycle.createInstance()) }!!
 
                             // verify whether the state of the current lifecycle model allows to run lifecycle:transition
                             val marking = findPath(lifecycleModel, transitions[component.lifecycleTransition!!]!!)
