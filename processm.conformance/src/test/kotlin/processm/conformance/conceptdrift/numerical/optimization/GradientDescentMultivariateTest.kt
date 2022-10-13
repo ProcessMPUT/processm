5ef12bcfb118731aa3d8fd62af2c1d6db048dd53
Jedrzej Potoniec <jpotoniec@cs.put.poznan.pl>
2022-10-13 08:25:32 +0200

    fix: (processm.conformance.conceptdrift.numerical.optimization.GradientDescentMultivariateTest) fixing a broken test (#144)
    Former-commit-id: 84243a43f0212aa892eb819cbff11cf8f3661fde

--- processm.conformance/src/test/kotlin/processm/conformance/conceptdrift/numerical/optimization/GradientDescentMultivariateTest.kt
+++ processm.conformance/src/test/kotlin/processm/conformance/conceptdrift/numerical/optimization/GradientDescentMultivariateTest.kt
@@ -1,5 +1,5 @@
 package processm.conformance.conceptdrift.numerical.optimization
 
 class GradientDescentMultivariateTest : MultivariateOptimizerTest() {
-    override fun instance() = GradientDescent()
+    override fun instance() = GradientDescent(eps = 1e-6)
 }
