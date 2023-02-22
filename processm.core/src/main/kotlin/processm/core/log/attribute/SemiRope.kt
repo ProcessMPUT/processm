7b415c51cf97b16f1029bd1643c2023280911eb7
Jedrzej Potoniec <jpotoniec@cs.put.poznan.pl>
2023-02-22 10:31:57 +0100

    chore: (processm.core.log.attribute.SemiRope) a better comparator (#151)
    Former-commit-id: be13c9c94ac99d50d6b5f7790507a2bd642f1edd

--- processm.core/src/main/kotlin/processm/core/log/attribute/SemiRope.kt
+++ processm.core/src/main/kotlin/processm/core/log/attribute/SemiRope.kt
@@ -2,12 +2,13 @@ package processm.core.log.attribute
 
 /**
  * Not really a rope, rather a recursive sequence of strings, each consisting of a prefix and a suffix.
- * While the implementation is general, the primary usage assumes the prefixes are shared between multiple instances
+ * While the implementation is general, the primary usage assumes the prefixes are shared between multiple instances,
  * and thus it makes sense to compare their address rather than their content. If this fails, a fallback is made to
  * implementations based on converting to strings as the common denominator.
  */
 class SemiRope(val left: CharSequence, val right: String) : CharSequence, Comparable<CharSequence> {
     override val length: Int = left.length + right.length
+    val depth: Int = if (left is SemiRope) left.depth + 1 else 2
 
     override fun get(index: Int): Char = if (index < left.length) left[index] else right[index - left.length]
 
@@ -20,10 +21,70 @@ class SemiRope(val left: CharSequence, val right: String) : CharSequence, Compar
             SemiRope(left.subSequence(startIndex, left.length), right.substring(0, endIndex - left.length))
         }
 
+
     override fun hashCode(): Int = 31 * left.hashCode() + right.hashCode()
+
+    private fun flattenTo(target: ArrayDeque<CharSequence>): ArrayDeque<CharSequence> {
+        if (right.isNotEmpty())
+            target.addFirst(right)
+        if (left is SemiRope)
+            left.flattenTo(target)
+        else {
+            if (left.isNotEmpty()) target.addFirst(left)
+        }
+        return target
+    }
+
+
     override fun compareTo(other: CharSequence): Int {
         if (other is SemiRope) {
             if (left === other.left) return right.compareTo(other.right)
+            val a = flattenTo(ArrayDeque(depth))
+            val b = other.flattenTo(ArrayDeque(other.depth))
+            var i = 0
+            var j = 0
+            while (i < a.size && j < b.size) {
+                if (a[i] != b[j])
+                    break
+                ++i
+                ++j
+            }
+            if (i == a.size) {
+                //a is a prefix of b, possibly an improper prefix, i.e., equal to b
+                return if (j == b.size) 0 else -1
+            }
+            if (j == b.size) {
+                //b is a proper prefix of a (since a==b was already handled)
+                return 1
+            }
+            var x = 0
+            var y = 0
+            var aEnded = false
+            var bEnded = false
+            while (!aEnded && !bEnded) {
+                val c = a[i][x].compareTo(b[j][y])
+                if (c != 0) return c
+                ++x
+                while (x == a[i].length) {
+                    ++i
+                    if (i == a.size) {
+                        aEnded = true
+                        break
+                    }
+                    x = 0
+                }
+                ++y
+                while (y == b[j].length) {
+                    ++j
+                    if (j == b.size) {
+                        bEnded = true
+                        break
+                    }
+                    y = 0
+                }
+            }
+            assert(aEnded || bEnded)
+            return bEnded.compareTo(aEnded)
         }
         return toString().compareTo(other.toString())
     }
--- processm.core/src/test/kotlin/processm/core/log/attribute/SemiRopeTest.kt
+++ processm.core/src/test/kotlin/processm/core/log/attribute/SemiRopeTest.kt
@@ -1,5 +1,6 @@
 package processm.core.log.attribute
 
+import kotlin.math.sign
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -47,5 +48,45 @@ class SemiRopeTest {
         assertTrue { ab_cde < ab_cde_fgh }
         assertTrue { ab_cde_fgh < ab_cde_fghi }
         assertTrue { ab_cde_fghi < ab_cde_fghj }
+    }
+
+    private fun ropify(s: String): Sequence<SemiRope> = sequence {
+        yield(SemiRope("", s))
+        if (s.length > 1) {
+            for (i in 1 until s.length) {
+                val leftString = s.substring(0, i)
+                val right = s.substring(i)
+                yieldAll(ropify(leftString).map { left -> SemiRope(left, right) })
+            }
+        }
+    }
+
+    private fun CharSequence.toDebugString(): String =
+        if (this is SemiRope) "${left.toDebugString()} -> $right" else toString()
+
+    @Test
+    fun `test ropify`() {
+        val s = "abcd"
+        val ropes = ropify(s).toList()
+        // there can be a cut between any two characters
+        assertEquals((1 shl (s.length - 1)), ropes.size)
+        for (rope in ropes)
+            assertEquals(s, rope.toString())
+    }
+
+    @Test
+    fun compareTo() {
+        val strings = listOf("a", "aa", "abb", "abcc", "abcd", "abd", "ac", "b").sorted()
+        repeat(1) {
+            for (s1 in strings) {
+                for (s2 in strings) {
+                    for (rope1 in ropify(s1)) {
+                        for (rope2 in ropify(s2)) {
+                            assertEquals(rope1.compareTo(rope2).sign, s1.compareTo(s2).sign)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
