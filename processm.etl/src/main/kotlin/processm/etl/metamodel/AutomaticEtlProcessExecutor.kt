d0d0818cf453f84b9fcb205fae9209df9784d5de
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2023-12-14 09:11:03 +0100

    chore: (processm.core) exposing attributeFromRecord as a public API to remove a copy in processm.etl (#175)
    Former-commit-id: 67fd6f7eeda598148482918c84692da509b2c8ad

--- processm.etl/src/main/kotlin/processm/etl/metamodel/AutomaticEtlProcessExecutor.kt
+++ processm.etl/src/main/kotlin/processm/etl/metamodel/AutomaticEtlProcessExecutor.kt
@@ -179,28 +179,6 @@ class AutomaticEtlProcessExecutor(
             objectData.forEach { (k, v) -> set("${DB_ATTR_NS}:$k", v) }
         })
 
-    //FIXME A copy from DBHierarchicalXESInputStream. I'd rather share it somehow
-    private fun attributeFromRecord(record: ResultSet): Any {
-        with(record) {
-            val type = getString("type")!!
-            assert(type.length >= 2)
-            return when (type[0]) {
-                's' -> getString("string_value")
-                'f' -> getDouble("real_value")
-                'i' -> when (type[1]) {
-                    'n' -> getLong("int_value")
-                    'd' -> getString("uuid_value").toUUID()!!
-                    else -> throw IllegalStateException("Invalid attribute type ${getString("type")} in the database.")
-                }
-
-                'd' -> getTimestamp("date_value", gmtCalendar).toInstant()
-                'b' -> getBoolean("bool_value")
-                'l' -> AttributeMap.LIST_TAG
-                else -> throw IllegalStateException("Invalid attribute type ${getString("type")} in the database.")
-            }
-        }
-    }
-
     private fun Connection.retrievePastEvents(objects: Collection<RemoteObjectID>): List<Event> {
         assert(objects.isNotEmpty())
         // This seems to be more efficient than using PQL
@@ -508,7 +486,6 @@ class AutomaticEtlProcessExecutor(
 
 
     companion object {
-        private val gmtCalendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"))
 
         val DB_ATTR_NS = "db"
         val INTERNAL_NS = "${Brand.mainDBInternalName}:internal"
