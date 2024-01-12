9f615bec53675848be1f026f99a626085a1ec9c7
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2024-01-12 16:17:57 +0100

    chore: (processm.launcher) try to use ffmpeg to compose the final recording of a Selenium test (#175)
    Former-commit-id: 9998d9228fd2892d86873219c2624ba3e1ac0562

--- processm.launcher/src/test/kotlin/processm/selenium/VideoRecorder.kt
+++ processm.launcher/src/test/kotlin/processm/selenium/VideoRecorder.kt
@@ -4,6 +4,8 @@ import org.openqa.selenium.OutputType
 import org.openqa.selenium.TakesScreenshot
 import java.nio.file.FileSystems
 import java.nio.file.Files
+import kotlin.io.path.deleteIfExists
+import kotlin.io.path.listDirectoryEntries
 
 /**
  * A helper to take screenshots while executing a Selenium suite of tests and then to instruct the user how to create a video out of them.
@@ -19,12 +21,24 @@ class VideoRecorder(private val obj: TakesScreenshot) {
     }
 
     fun finish() {
+        val videoFile = "output.mp4"
         println()
         println()
-        println()
-        println("Screenshots were saved to $target")
-        println("Go there and run: ffmpeg -framerate 2 -i %04d.png output.mp4")
-        println()
+        try {
+            val process = ProcessBuilder("ffmpeg", "-framerate", "2", "-i", "%04d.png", videoFile)
+                .directory(target.toFile())
+                .start()
+            if (process.waitFor() != 0)
+                throw RuntimeException("ffmpeg error")
+            println("Video was saved to $target/$videoFile")
+            for (f in target.listDirectoryEntries("*.png"))
+                f.deleteIfExists()
+        } catch (e: Exception) {
+            println("Composing the final video failed. Make sure `ffmpeg` is installed and available.")
+            println("Screenshots were saved to $target")
+            println("Go there and run: ffmpeg -framerate 2 -i %04d.png output.mp4")
+            e.printStackTrace()
+        }
         println()
         println()
     }
