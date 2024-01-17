af574661945e4616f05199859198fd2319d2a046
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2024-01-17 14:53:53 +0100

    Revert "dont merge me"
    This reverts commit 0c87e9af6eadbccc3d3ba1ce1490b104fde71f25 [formerly 72c7ff4d5b9be981ada6f66a23e7f6139c58064f].
    
    
    Former-commit-id: aca16dbbfa7899c671ed8c6187b2a51c868b7a0b

--- processm.core/src/test/kotlin/processm/core/DoSomething.kt
+++ processm.core/src/test/kotlin/processm/core/DoSomething.kt
@@ -1,7 +1,9 @@
 package processm.core
 
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeDriverService
 import org.openqa.selenium.chrome.ChromeOptions
+import java.io.File
 import java.time.Duration
 import kotlin.test.Test
 
@@ -9,7 +11,9 @@ class DoSomething {
 
     @Test
     fun test() {
-        val driver = ChromeDriver(ChromeOptions().apply {
+        val service =
+            ChromeDriverService.Builder().usingDriverExecutable(File("/home/processm/pseudochrome.sh")).build()
+        val driver = ChromeDriver(service, ChromeOptions().apply {
             addArguments("--window-size=1920,1080")
             addArguments("--headless=new")
         })
