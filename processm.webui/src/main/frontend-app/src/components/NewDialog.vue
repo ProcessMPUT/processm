7644f5d9f787cf9bf652a6e021c27a182564b169
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2024-01-29 18:01:19 +0100

    chore: (processm.webui) NewDialog (#190)
    Perhaps it'd be good to merge it with RenameDialog?
    
    
    Former-commit-id: cc0d2b7ee73ad6605e6b92f67f2e4b191026d165

---
+++ processm.webui/src/main/frontend-app/src/components/NewDialog.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-dialog v-model="value" @click:outside="cancel" max-width="400">
+    <v-card>
+      <v-card-title class="headline">
+        {{ $t("new-dialog.dialog-title") }}
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-form v-model="isNewNameValid" ref="renameForm">
+            <v-row>
+              <v-col>
+                <v-text-field
+                    v-model="newName"
+                    :label="$t('new-dialog.new-name-placeholder')"
+                    :rules="[
+                    (v) => !!v || $t('new-dialog.validation.name-empty')
+                  ]"
+                    required
+                />
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-container>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn color="primary darken-1" text @click.stop="cancel">
+          {{ $t("common.cancel") }}
+        </v-btn>
+
+        <v-btn
+            color="primary darken-1"
+            text
+            :disabled="!isNewNameValid"
+            @click.stop="save"
+        >
+          {{ $t("common.save") }}
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import {Component, Prop} from "vue-property-decorator";
+
+@Component
+export default class RenameDialog extends Vue {
+  @Prop({default: false})
+  readonly value!: boolean;
+
+  isNewNameValid = true;
+  newName = "";
+
+  cancel() {
+    this.$emit("cancelled");
+    this.resetForm();
+  }
+
+  save() {
+    this.$emit("submitted", this.newName);
+    this.resetForm();
+  }
+
+  private resetForm() {
+    (this.$refs.renameForm as HTMLFormElement).reset();
+  }
+}
+</script>
