9410e2a167f776dbee62fa00b2e941091438e334
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2024-02-09 12:15:28 +0100

    dont merge me
    Former-commit-id: 0725a85a8f07e62ed4549f2d45dc3da44cb7dd1d

--- processm.conformance/src/test/kotlin/processm/conformance/conceptdrift/numerical/integration/MidpointIntegratorTest.kt
+++
@@ -1,5 +0,0 @@
-package processm.conformance.conceptdrift.numerical.integration
-
-class MidpointIntegratorTest : IntegratorTest() {
-    override fun instance(): Integrator = MidpointIntegrator(1e-4)
-}
