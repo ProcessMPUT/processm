18c4aa4dcf2a02e262fb9c57cc24abb17cc31545
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2024-02-23 16:14:40 +0100

    chore: (processm.webui.views.OrganizationList) reforamt (#190)
    Former-commit-id: ef7901aff69eb6ffc9a6de4f76b6eb65d34bf97b

--- processm.webui/src/main/frontend-app/src/views/OrganizationList.vue
+++ processm.webui/src/main/frontend-app/src/views/OrganizationList.vue
@@ -1,19 +1,17 @@
 <template>
-
   <v-card>
     <v-card-title>
       <v-toolbar flat>
         <v-toolbar-title> {{ $t("users.organizations") }}</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn color="primary" @click.stop="createNewDialog=true;">
+        <v-btn color="primary" @click.stop="createNewDialog = true">
           {{ $t("common.add-new") }}
         </v-btn>
       </v-toolbar>
     </v-card-title>
     <v-card-text>
-      <v-treeview :items="organizations" item-children="children" item-key="organization.id"
-                  item-text="organization.name">
-        <template v-slot:prepend="{item}">
+      <v-treeview :items="organizations" item-children="children" item-key="organization.id" item-text="organization.name">
+        <template v-slot:prepend="{ item }">
           <v-tooltip bottom>
             <template v-slot:activator="{ on, attrs }">
               <v-btn icon @click.stop="togglePrivate(item)" v-bind="attrs" v-on="on">
@@ -25,11 +23,10 @@
             <span v-else>{{ $t("organizations.switch-to-private") }}</span>
           </v-tooltip>
         </template>
-        <template v-slot:append="{item}">
+        <template v-slot:append="{ item }">
           <v-tooltip bottom>
             <template v-slot:activator="{ on, attrs }">
-              <v-btn icon v-if="item.organization.parentOrganizationId !== undefined && item.canDetach"
-                     @click.stop="detach(item)" v-bind="attrs" v-on="on">
+              <v-btn icon v-if="item.organization.parentOrganizationId !== undefined && item.canDetach" @click.stop="detach(item)" v-bind="attrs" v-on="on">
                 <v-icon>arrow_upward</v-icon>
               </v-btn>
             </template>
@@ -61,42 +58,40 @@
           </v-tooltip>
           <v-tooltip bottom>
             <template v-slot:activator="{ on, attrs }">
-              <v-btn icon v-if="item.canLogin && item.organization.id != $sessionStorage.currentOrganization.id"
-                     v-bind="attrs" v-on="on">
+              <v-btn icon v-if="item.canLogin && item.organization.id != $sessionStorage.currentOrganization.id" v-bind="attrs" v-on="on">
                 <v-icon small @click="login(item)">input</v-icon>
               </v-btn>
             </template>
             <span>{{ $t("organizations.login") }}</span>
           </v-tooltip>
         </template>
-        <template v-slot:label="{item}">
-          <v-btn v-if="orgToAttach!==null && item.organization.id != orgToAttach.id && item.canAttachTo"
-                 @click.stop="endAttach(item)">
+        <template v-slot:label="{ item }">
+          <v-btn v-if="orgToAttach !== null && item.organization.id != orgToAttach.id && item.canAttachTo" @click.stop="endAttach(item)">
             {{ item.organization.name }}
           </v-btn>
           <v-text-field
-              v-model="item.organization.name"
-              background-color="transparent"
-              flat
-              hide-details
-              solo
-              @blur="item.focus = false"
-              @change="item.dirty = true"
-              @focus="item.focus = true"
-              v-else
+            v-model="item.organization.name"
+            background-color="transparent"
+            flat
+            hide-details
+            solo
+            @blur="item.focus = false"
+            @change="item.dirty = true"
+            @focus="item.focus = true"
+            v-else
           >
             <template v-slot:append>
               <v-tooltip bottom>
                 <template v-slot:activator="{ on, attrs }">
                   <v-btn
-                      :disabled="!((item.dirty || item.focus) && item.canEdit)"
-                      color="primary"
-                      dark
-                      icon
-                      v-bind="attrs"
-                      @click="editName(item)"
-                      v-on="on"
-                      @keyup.enter.native="editName(item)"
+                    :disabled="!((item.dirty || item.focus) && item.canEdit)"
+                    color="primary"
+                    dark
+                    icon
+                    v-bind="attrs"
+                    @click="editName(item)"
+                    v-on="on"
+                    @keyup.enter.native="editName(item)"
                   >
                     <v-icon v-show="(item.dirty || item.focus) && item.canEdit" small>edit</v-icon>
                   </v-btn>
@@ -107,19 +102,25 @@
           </v-text-field>
         </template>
       </v-treeview>
-      <new-dialog :value="createNewDialog" @submitted="endCreate"
-                  @cancelled="createNewDialog=false; orgToAttach=null;"/>
+      <new-dialog
+        :value="createNewDialog"
+        @submitted="endCreate"
+        @cancelled="
+          createNewDialog = false;
+          orgToAttach = null;
+        "
+      />
     </v-card-text>
   </v-card>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import {Component, Inject} from "vue-property-decorator";
+import { Component, Inject } from "vue-property-decorator";
 import OrganizationService from "@/services/OrganizationService";
 import AccountService from "@/services/AccountService";
 import App from "@/App.vue";
-import {Organization, OrganizationRole} from "@/openapi";
+import { Organization, OrganizationRole } from "@/openapi";
 import NewDialog from "@/components/NewDialog.vue";
 
 class OrganizationTreeItem {
@@ -136,7 +137,7 @@ class OrganizationTreeItem {
 }
 
 @Component({
-  components: {NewDialog}
+  components: { NewDialog }
 })
 export default class OrganizationList extends Vue {
   @Inject() app!: App;
@@ -166,8 +167,7 @@ export default class OrganizationList extends Vue {
   async load() {
     const perm: { [id: string]: OrganizationRole } = {};
     for (const item of this.$sessionStorage.userOrganizations) {
-      if (item.organization.id !== undefined)
-        perm[item.organization.id] = item.role;
+      if (item.organization.id !== undefined) perm[item.organization.id] = item.role;
     }
     const organizations = await this.organizationService.getOrganizations();
     const items: { [id: string]: OrganizationTreeItem } = {};
@@ -194,10 +194,9 @@ export default class OrganizationList extends Vue {
         items[org.parentOrganizationId].children.push(items[org.id]);
       }
     }
-    this.organizations = Object
-        .values(items)
-        .filter((org) => org.organization?.parentOrganizationId === undefined)
-        .sort((a, b) => (a.organization?.name ?? "").localeCompare(b.organization?.name ?? ""))
+    this.organizations = Object.values(items)
+      .filter((org) => org.organization?.parentOrganizationId === undefined)
+      .sort((a, b) => (a.organization?.name ?? "").localeCompare(b.organization?.name ?? ""));
   }
 
   async beginAttach(item: OrganizationTreeItem) {
@@ -206,41 +205,41 @@ export default class OrganizationList extends Vue {
 
   async endAttach(item: OrganizationTreeItem) {
     try {
-      const subOrganizationId = this.orgToAttach?.id
-      const organizationId = item.organization?.id
-      await this.organizationService.attach(organizationId!, subOrganizationId!)
-      this.app.success(this.$t("organizations.moved").toString())
-      await this.load()
+      const subOrganizationId = this.orgToAttach?.id;
+      const organizationId = item.organization?.id;
+      await this.organizationService.attach(organizationId!, subOrganizationId!);
+      this.app.success(this.$t("organizations.moved").toString());
+      await this.load();
     } catch (e) {
-      console.error(e)
-      this.app.error(this.$t("common.operation-error").toString())
+      console.error(e);
+      this.app.error(this.$t("common.operation-error").toString());
     } finally {
-      this.orgToAttach = null
+      this.orgToAttach = null;
     }
   }
 
   async detach(item: OrganizationTreeItem) {
     try {
-      const organizationId = item.organization?.parentOrganizationId
-      const subOrganizationId = item.organization?.id
-      await this.organizationService.detach(organizationId!, subOrganizationId!)
-      this.app.success(this.$t("organizations.moved").toString())
-      await this.load()
+      const organizationId = item.organization?.parentOrganizationId;
+      const subOrganizationId = item.organization?.id;
+      await this.organizationService.detach(organizationId!, subOrganizationId!);
+      this.app.success(this.$t("organizations.moved").toString());
+      await this.load();
     } catch (e) {
-      console.error(e)
-      this.app.error(this.$t("common.operation-error").toString())
+      console.error(e);
+      this.app.error(this.$t("common.operation-error").toString());
     }
   }
 
   async remove(item: OrganizationTreeItem) {
     try {
-      const organizationId = item.organization?.id
-      await this.organizationService.removeOrganization(organizationId!)
-      this.app.success(this.$t("common.removal.success").toString())
-      await this.load()
+      const organizationId = item.organization?.id;
+      await this.organizationService.removeOrganization(organizationId!);
+      this.app.success(this.$t("common.removal.success").toString());
+      await this.load();
     } catch (e) {
-      console.error(e)
-      this.app.error(this.$t("common.removal.failure").toString())
+      console.error(e);
+      this.app.error(this.$t("common.removal.failure").toString());
     }
   }
 
@@ -251,14 +250,13 @@ export default class OrganizationList extends Vue {
 
   async endCreate(name: string) {
     try {
-      const newOrg = (await this.organizationService.createOrganization(name, false))?.id!
-      const parentOrgId = this.orgToAttach?.id
-      if (parentOrgId !== undefined)
-        await this.organizationService.attach(parentOrgId, newOrg)
-      await this.load()
-      this.app.success(this.$t("common.creating.success").toString())
+      const newOrg = (await this.organizationService.createOrganization(name, false))?.id!;
+      const parentOrgId = this.orgToAttach?.id;
+      if (parentOrgId !== undefined) await this.organizationService.attach(parentOrgId, newOrg);
+      await this.load();
+      this.app.success(this.$t("common.creating.success").toString());
     } catch (e) {
-      this.app.error(this.$t("common.creating.error").toString())
+      this.app.error(this.$t("common.creating.error").toString());
     } finally {
       this.createNewDialog = false;
       this.orgToAttach = null;
@@ -267,24 +265,24 @@ export default class OrganizationList extends Vue {
 
   async togglePrivate(item: OrganizationTreeItem) {
     try {
-      const org = item.organization!
-      await this.organizationService.updateOrganization(org.id!, org.name, !org.isPrivate)
-      await this.load()
-      this.app.success(this.$t("common.saving.success").toString())
+      const org = item.organization!;
+      await this.organizationService.updateOrganization(org.id!, org.name, !org.isPrivate);
+      await this.load();
+      this.app.success(this.$t("common.saving.success").toString());
     } catch (e) {
-      this.app.error(this.$t("common.saving.error").toString())
+      this.app.error(this.$t("common.saving.error").toString());
     }
   }
 
   async editName(item: OrganizationTreeItem) {
     try {
-      const org = item.organization!
+      const org = item.organization!;
       //org.name contains the new name
-      await this.organizationService.updateOrganization(org.id!, org.name, org.isPrivate)
-      await this.load()
-      this.app.success(this.$t("common.saving.success").toString())
+      await this.organizationService.updateOrganization(org.id!, org.name, org.isPrivate);
+      await this.load();
+      this.app.success(this.$t("common.saving.success").toString());
     } catch (e) {
-      this.app.error(this.$t("common.saving.error").toString())
+      this.app.error(this.$t("common.saving.error").toString());
     }
   }
 
