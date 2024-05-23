b40dea1d4bb17bbfb219ebc508bd9e453add4722
tpawlak <tpawlak@cs.put.poznan.pl>
2024-05-23 17:44:13 +0200

    feat: tooltips with KPIs for graph nodes (#228)
    Former-commit-id: a2119227d3f99bf714d5cd77a7a3aa2318df561f

--- processm.services/src/main/kotlin/processm/services/JsonSerializer.kt
+++ processm.services/src/main/kotlin/processm/services/JsonSerializer.kt
@@ -4,6 +4,7 @@ package processm.services
 
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
@@ -12,10 +13,12 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.*
 import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.overwriteWith
 import kotlinx.serialization.serializer
 import processm.dbmodels.models.ComponentTypeDto
 import processm.helpers.UUIDSerializer
 import processm.helpers.serialization.SerializersModuleProvider
+import processm.helpers.stats.Distribution
 import processm.services.api.models.BPMNComponentData
 import processm.services.api.models.CausalNetComponentData
 import processm.services.api.models.DirectlyFollowsGraphComponentData
@@ -36,7 +39,9 @@ val JsonSerializer = Json {
         contextual(Any::class, AnySerializer as KSerializer<Any>)
         contextual(LocalDateTime::class, LocalDateTimeSerializer)
         contextual(UUID::class, UUIDSerializer)
-    }
+    }.overwriteWith(SerializersModule {
+        contextual(Distribution::class, DistributionWebAPISerializer)
+    })
 }
 
 private object AnySerializer : KSerializer<Any?> {
@@ -99,6 +104,36 @@ private object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
 
     override fun serialize(encoder: Encoder, value: LocalDateTime) {
         encoder.encodeString(value.toString())
+    }
+
+}
+
+private object DistributionWebAPISerializer : KSerializer<Distribution> {
+    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("DistributionWebAPI") {
+        element("min", Double.serializer().descriptor)
+        element("Q1", Double.serializer().descriptor)
+        element("median", Double.serializer().descriptor)
+        element("Q3", Double.serializer().descriptor)
+        element("max", Double.serializer().descriptor)
+        element("average", Double.serializer().descriptor)
+        element("standardDeviation", Double.serializer().descriptor)
+    }
+
+    override fun deserialize(decoder: Decoder): Distribution {
+        throw UnsupportedOperationException()
+    }
+
+    override fun serialize(encoder: Encoder, value: Distribution) {
+        encoder as JsonEncoder
+        encoder.beginStructure(descriptor)
+        encoder.encodeDoubleElement(descriptor, 0, value.min)
+        encoder.encodeDoubleElement(descriptor, 1, value.Q1)
+        encoder.encodeDoubleElement(descriptor, 2, value.median)
+        encoder.encodeDoubleElement(descriptor, 3, value.Q3)
+        encoder.encodeDoubleElement(descriptor, 4, value.max)
+        encoder.encodeDoubleElement(descriptor, 5, value.average)
+        encoder.encodeDoubleElement(descriptor, 6, value.standardDeviation)
+        encoder.endStructure(descriptor)
     }
 
 }
--- processm.webui/src/main/frontend-app/src/components/Graph.vue
+++ processm.webui/src/main/frontend-app/src/components/Graph.vue
@@ -9,16 +9,34 @@
 }
 </style>
 
+<style>
+.graph .g6-component-tooltip {
+  max-width: 50em;
+}
+
+.graph .g6-component-tooltip table {
+  margin: 0;
+  padding: 0;
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+
+.graph .g6-component-tooltip td {
+  padding: 0;
+}
+</style>
+
 <script lang="ts">
 import { Component, Prop, Vue, Watch } from "vue-property-decorator";
 import G6, { GraphData, IGroup } from "@antv/g6";
 import { EdgeConfig, NodeConfig } from "@antv/g6-core/lib/types";
 import { waitForRepaint } from "@/utils/waitForRepaint";
+import { AlignmentKPIReport, Distribution } from "@/openapi";
 
 @Component({})
 export default class Graph extends Vue {
   @Prop()
-  readonly data!: GraphData | CNetGraphData;
+  readonly data!: (GraphData | CNetGraphData) & AlignmentKPIHolder;
 
   @Prop({ default: (edge: EdgeConfig) => true })
   readonly filterEdge!: (edge: EdgeConfig) => boolean;
@@ -36,17 +54,82 @@ export default class Graph extends Vue {
   mounted() {
     // eslint-disable-next-line @typescript-eslint/no-this-alias
     const self = this;
+
+    const numberFormat = Intl.NumberFormat(self.$i18n.locale, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    });
+    const intFormat = Intl.NumberFormat(self.$i18n.locale, {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0
+    });
+    const dateFormat = Intl.DateTimeFormat(self.$i18n.locale);
+
     // wait until height of the component is calculated
     waitForRepaint(() => {
       // get the container size
       const container = this.$refs.graph as HTMLElement;
       if (container === undefined || self.data === undefined) return;
+      const tooltip = new G6.Tooltip({
+        offsetX: 10,
+        offsetY: 10,
+        itemTypes: ["node", "edge"],
+        getContent: (e) => {
+          const outDiv = document.createElement("div");
+          // TODO
+          const type = e?.item?.getType();
+          const model = e?.item?.getModel();
+          if (model === undefined || model.id === undefined) return outDiv;
+
+          const format = (kpi: string, d: Distribution): string => {
+            if (kpi == "time:timestamp") {
+              const f = (d: number) => dateFormat.format(new Date(d));
+              const days = d.standardDeviation! / 86400000;
+              return `${f(d.average!)} &pm; ${numberFormat.format(days)} ${self.$t("statistics.days")} [${f(d.min!)}, ${f(d.max!)}]`;
+            } else if (kpi == "urn:processm:statistics/count") {
+              return intFormat.format(d.median!);
+            } else {
+              const f = (d: number) => numberFormat.format(d);
+              const days = self.$t("statistics.days");
+              return `${f(d.average!)} ${days} &pm; ${f(d.standardDeviation!)} ${days} [${f(d.min!)} ${days}, ${f(d.max!)} ${days}]`;
+            }
+          };
+
+          switch (type) {
+            case "node":
+              outDiv.innerHTML = `<h4>${model.label}</h4>`;
+              if (self.data.alignmentKPIReport === undefined) break;
+
+              outDiv.innerHTML +=
+                "<table>" +
+                Object.entries(self.data.alignmentKPIReport.eventKPI)
+                  .map((kpi) => {
+                    const label = self.$te(`statistics.${kpi[0]}`) ? self.$t(`statistics.${kpi[0]}`) : kpi[0];
+                    const val = kpi[1][model.id!];
+                    console.log(JSON.stringify(val));
+                    if (val !== undefined) return `<tr><td>${label}:</td><td>${format(kpi[0], val)}</td></tr>`;
+                    else return "";
+                  })
+                  .join("") +
+                "</table>";
+              break;
+            case "edge":
+              outDiv.innerHTML = `<h4>${model.label}</h4>`;
+              break;
+          }
+          return outDiv;
+        }
+      });
       const graph = new G6.Graph({
         container: container, // String | HTMLElement, required, the id of DOM element or an HTML node
         width: container.offsetWidth, // Number, required, the width of the graph
         height: container.offsetHeight, // Number, required, the height of the graph
         fitView: true,
         fitCenter: true,
+        plugins: [tooltip],
+        modes: {
+          default: ["drag-node"]
+        },
         layout: {
           type: "comboCombined",
           preventOverlap: true,
@@ -332,5 +415,9 @@ export interface CNetGraphData extends GraphData {
 export interface CNetNodeConfig extends NodeConfig {
   joins: string[][];
   splits: string[][];
+}
+
+export interface AlignmentKPIHolder {
+  alignmentKPIReport?: AlignmentKPIReport;
 }
 </script>
