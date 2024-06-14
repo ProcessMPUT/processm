921cf3dd8b3cbdcc3b543d74448be867faeb537e
tpawlak <tpawlak@cs.put.poznan.pl>
2024-06-14 22:55:32 +0200

    fix: tests (#227)
    Former-commit-id: 56daf57d02e21a335ad51d29ef1185ee8b426d5d

--- processm.launcher/src/test/kotlin/processm/IntegrationTests.kt
+++ processm.launcher/src/test/kotlin/processm/IntegrationTests.kt
@@ -296,7 +296,7 @@ SELECT "concept:name", "lifecycle:transition", "concept:instance", "time:timesta
                 val re = Regex("[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}", RegexOption.IGNORE_CASE)
                 val startTime = System.currentTimeMillis()
                 val token = runBlocking {
-                    for (i in 1..40) {
+                    for (i in 1..100) {
                         try {
                             re.find(sendmail.lastEmailBody)?.value?.let {
                                 logger.info("Email sent in ${System.currentTimeMillis() - startTime}ms")
