40369218ee973419b841a2a8082bdce6d125a5a0
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2024-08-21 11:48:52 +0200

    freat: (processm.launcher) Docker image with Timescale and exposed volumes (#211)
    Former-commit-id: 2cc5bf49e13e48c5c1709ab47fb130c727cb6913

---
+++ docker-entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+CONFIG_FILE=/processm/conf/config.properties
+SLEEP=1
+REPETITIONS=10
+
+if [ -n "$POSTGRES_PASSWORD" ]
+then
+  # := is used to set the varabiles if they are not set. This ensures that the DB initialization script will not use different defaults
+  export POSTGRES_DB=${POSTGRES_DB:=processm}
+  export POSTGRES_USER=${POSTGRES_USER:=postgres}
+  url="jdbc:postgresql://localhost:5432/$POSTGRES_DB?user=$POSTGRES_USER&password=$POSTGRES_PASSWORD"
+  tmp=$(mktemp)
+  (sed 's/^processm.core.persistence.connection.URL[^[:alnum:]].*$/#&/gi' <"$CONFIG_FILE"; echo "processm.core.persistence.connection.URL=$url") >"$tmp"
+  mv "$tmp" "$CONFIG_FILE"
+fi
+
+url=$(grep '^processm.core.persistence.connection.URL[^[:alnum:]]' <"$CONFIG_FILE" |tail -n 1|sed 's/^[^=]*=[[:space:]]*jdbc://')
+
+/usr/local/bin/docker-entrypoint.sh postgres &
+
+for i in $(seq 1 $REPETITIONS)
+do
+  if psql "$url" </dev/null
+  then
+    exec java -Xmx8G -jar "launcher-$PROCESSM_VERSION.jar"
+  else
+    sleep $SLEEP
+  fi
+done
+
+echo "The database is not available at $url. Terminating."
+exit 1
