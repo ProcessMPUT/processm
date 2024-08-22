19299332f83cc23035ca9f0e7124d31db1c177af
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2024-08-22 17:17:25 +0200

    chore: (processm.launcher) using the official starting script in docker-start-process.sh (#211)
    Former-commit-id: 7fade528e3a59bf873b60e21e92cb29413ed8280

--- processm.launcher/src/main/scripts/processm.sh
+++ processm.launcher/src/main/scripts/processm.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
-cd "${0%/*}"
+#!/bin/sh
+# Make sure any modifications are compatible with busybox, as the Docker image uses it.
+dir=$(dirname "$0")
+cd "$dir"
 java -Xmx8G -jar launcher-0.7.0.jar
