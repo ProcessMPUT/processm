d47e3275f6d9d047fa344b1561055227f0ddc6c2
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2024-08-23 12:31:10 +0200

    chore: (processm.launcher) more robust script (#211)
    Former-commit-id: bdcff67c8743f6b1deb6e07d3d4d75a2607e3eee

--- processm.launcher/src/main/scripts/processm.sh
+++ processm.launcher/src/main/scripts/processm.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+PROCESSM_VERSION=${PROCESSM_VERSION:=0.7.0}
 # Make sure any modifications are compatible with busybox, as the Docker image uses it.
 dir=$(dirname "$0")
 cd "$dir"
-java -Xmx8G -jar launcher-0.7.0.jar
+java -Xmx8G -jar "launcher-$PROCESSM_VERSION.jar"
