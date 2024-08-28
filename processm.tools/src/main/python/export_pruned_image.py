a899e5c12dcbc4ed2be8403708367a0a664ae844
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2024-08-28 13:56:19 +0200

    feat: (processm.tools) a tool for pruning exported Docker images (#211)
    Former-commit-id: 00df6b96cb48287caa2ec88609f3c801af3450d9

---
+++ processm.tools/src/main/python/export_pruned_image.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+import tarfile
+
+
+def get_blobs(image: str) -> list[str]:
+    blobs = []
+    with subprocess.Popen(["docker", "save", image], stdout=subprocess.PIPE) as proc:
+        with tarfile.open(fileobj=proc.stdout, mode="r|") as tar:
+            blobs += [member.name for member in tar.getmembers() if
+                      member.isfile() and member.name.startswith("blobs/")]
+    return blobs
+
+
+def exclude_blobs(image: str, exclude: list[str]):
+    with subprocess.Popen(["docker", "save", image], stdout=subprocess.PIPE) as docker, \
+            subprocess.Popen(["tar", "--delete"] + exclude, stdin=docker.stdout, stderr=subprocess.DEVNULL):
+        # This error redirection is ugly, but tar spams with information that a member was not found in the archive
+        pass
+
+
+def main(base_image: str, reference_images: list[str]):
+    blobs = []
+    for image in reference_images:
+        blobs += get_blobs(image)
+    print("Identified", len(blobs), "candidate blobs to remove", file=sys.stderr)
+    exclude_blobs(base_image, blobs)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 2:
+        main(sys.argv[1], sys.argv[2:])
+    else:
+        print("Usage:", sys.argv[0], "image-to-export", "image-to-remove...", file=sys.stderr)
+        sys.exit(1)
