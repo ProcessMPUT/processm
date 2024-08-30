4c8786eb3e8cd03d4316ad854d62815fdb3a9d74
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2024-08-30 10:21:40 +0200

    chore: (processm.tools) improving the Docker image with the generator (#283)
    Importing data is now done once, during the image build, and not in the
    entrypoint.
    
    
    Former-commit-id: ccceaf1e4d503c199d8ed0828dfab6a9347e2fdd

--- processm.tools/src/main/docker/wwi/entry.sh
+++ processm.tools/src/main/docker/wwi/entry.sh
@@ -6,16 +6,11 @@ export SQLCMDPASSWORD=$MSSQL_SA_PASSWORD
 SQLCMD="/opt/mssql-tools/bin/sqlcmd -U SA -I"
 
 /opt/mssql/bin/sqlservr --accept-eula --reset-sa-password &
-MSSQL_PID=$!
 
-for i in `seq 1 10`
+for i in $(seq 1 100)
 do
    $SQLCMD </dev/null && break
-   sleep 10
+   sleep 1
 done
 
-$SQLCMD -i /processm/load-wwi.sql || exit 1
-$SQLCMD -i /processm/create-procedures.sql || exit 1
-
-#exec sleep 666d
-exec java -D'processm.tools.generator.dbURL=jdbc:sqlserver://localhost:1433;database=WideWorldImporters' -D'processm.tools.generator.dbUser=SA' -D"processm.tools.generator.dbPassword=$MSSQL_SA_PASSWORD" -jar /processm/processm.tools-0.7.0-jar-with-dependencies.jar
+exec java -D'processm.tools.generator.dbURL=jdbc:sqlserver://localhost:1433;database=WideWorldImporters' -D'processm.tools.generator.dbUser=SA' -D"processm.tools.generator.dbPassword=$MSSQL_SA_PASSWORD" -jar "/processm/processm.tools-${PROCESSM_VERSION}-jar-with-dependencies.jar"
