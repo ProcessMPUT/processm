d66c3184f22c7b20b0bdba58f9112b4c10ebc046
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2024-10-24 17:27:54 +0200

    chore: (processm.conformance) optimizing HIrschbergAligner (#328)
    Former-commit-id: ce783c227e738b444379cfbad53ea35491850791

--- processm.conformance/src/main/kotlin/processm/conformance/models/antialignments/HirschbergAligner.kt
+++ processm.conformance/src/main/kotlin/processm/conformance/models/antialignments/HirschbergAligner.kt
@@ -8,6 +8,7 @@ import processm.conformance.models.alignments.Step
 import processm.core.log.Event
 import processm.core.log.hierarchical.Trace
 import processm.core.models.commons.Activity
+import java.util.*
 import kotlin.math.min
 
 
@@ -16,25 +17,27 @@ internal class HirschbergAligner(
     override val penalty: PenaltyFunction = PenaltyFunction()
 ) : Aligner {
 
-    private val Activity.deletionCost: Int
-        get() = if (isSilent) penalty.silentMove else penalty.modelMove
-
-    private val Event.insertionCost: Int
-        get() = penalty.logMove
+    init {
+        assert(penalty.synchronousMove == 0)
+        assert(penalty.silentMove == 0)
+    }
 
     internal fun nwScore(x: List<Activity>, y: List<Event>): IntArray {
         var s0 = IntArray(y.size + 1)
         var s1 = IntArray(y.size + 1)
         for (j in y.indices)
-            s0[j + 1] = s0[j] + y[j].insertionCost
+            s0[j + 1] = s0[j] + penalty.logMove
         for (i in x.indices) {
-            s1[0] = s0[0] + x[i].deletionCost
+            s1[0] = s0[0] + penalty.modelMove
             for (j in y.indices) {
-                val del = s0[j + 1] + x[i].deletionCost
-                val ins = s1[j] + y[j].insertionCost
-                s1[j + 1] = min(ins, del)
-                if (x[i].name == y[j].conceptName && !x[i].isSilent)
-                    s1[j + 1] = min(s1[j + 1], s0[j] + penalty.synchronousMove)
+                if (x[i].name == y[j].conceptName) {
+                    // always commit to the synchronous move
+                    s1[j + 1] = s0[j]
+                } else {
+                    val del = s0[j + 1] + penalty.modelMove
+                    val ins = s1[j] + penalty.logMove
+                    s1[j + 1] = min(ins, del)
+                }
             }
             s0 = s1.also { s1 = s0 }
             assert(s0 !== s1)
@@ -42,48 +45,37 @@ internal class HirschbergAligner(
         return s0
     }
 
-    internal fun hirschberg(x: List<Activity>, y: List<Event>): List<Step> {
+    internal fun hirschberg(target: MutableList<Step>, x: List<Activity>, y: List<Event>) {
         if (x.isEmpty()) {
-            return y.map { e -> Step(modelMove = null, logMove = e, type = DeviationType.LogDeviation) }
+            y.mapTo(target) { e -> Step(modelMove = null, logMove = e, type = DeviationType.LogDeviation) }
         } else if (y.isEmpty()) {
-            return x.map { a -> Step(modelMove = a, logMove = null, type = DeviationType.ModelDeviation) }
+            x.mapTo(target) { a -> Step(modelMove = a, logMove = null, type = DeviationType.ModelDeviation) }
         } else if (x.size == 1) {
             // Wiki says I am supposed to call NeedlemanWunsch here, but I think we have simple enough case?
             val a = x.single()
-            if (a.isSilent) {
-                return ArrayList<Step>().apply {
-                    add(Step(modelMove = a, logMove = null, type = DeviationType.ModelDeviation))
-                    y.mapTo(this) { e -> Step(modelMove = null, logMove = e, type = DeviationType.LogDeviation) }
-                }
-            } else {
-                assert(y.isNotEmpty())
-                return ArrayList<Step>().apply {
-                    var hit = false
-                    for (e in y)
-                        if (!hit && e.conceptName == a.name) {
-                            hit = true
-                            add(Step(modelMove = a, logMove = e, type = DeviationType.None))
-                        } else
-                            add(Step(modelMove = null, logMove = e, type = DeviationType.LogDeviation))
-                    if (!hit)
-                        add(Step(modelMove = a, logMove = null, type = DeviationType.ModelDeviation))
-                }
-            }
+            assert(y.isNotEmpty())
+            var hit = false
+            for (e in y)
+                if (!hit && e.conceptName == a.name) {
+                    hit = true
+                    target.add(Step(modelMove = a, logMove = e, type = DeviationType.None))
+                } else
+                    target.add(Step(modelMove = null, logMove = e, type = DeviationType.LogDeviation))
+            if (!hit)
+                target.add(Step(modelMove = a, logMove = null, type = DeviationType.ModelDeviation))
         } else if (y.size == 1) {
             // Wiki says I am supposed to call NeedlemanWunsch here, but I think we have simple enough case?
             assert(x.isNotEmpty())
-            return ArrayList<Step>().apply {
-                val e = y.single()
-                var hit = false
-                for (a in x)
-                    if (!hit && e.conceptName == a.name) {
-                        hit = true
-                        add(Step(modelMove = a, logMove = e, type = DeviationType.None))
-                    } else
-                        add(Step(modelMove = a, logMove = null, type = DeviationType.ModelDeviation))
-                if (!hit)
-                    add(Step(modelMove = null, logMove = e, type = DeviationType.LogDeviation))
-            }
+            val e = y.single()
+            var hit = false
+            for (a in x)
+                if (!hit && e.conceptName == a.name) {
+                    hit = true
+                    target.add(Step(modelMove = a, logMove = e, type = DeviationType.None))
+                } else
+                    target.add(Step(modelMove = a, logMove = null, type = DeviationType.ModelDeviation))
+            if (!hit)
+                target.add(Step(modelMove = null, logMove = e, type = DeviationType.LogDeviation))
         } else {
             assert(x.size > 1)
             assert(y.size > 1)
@@ -91,24 +83,42 @@ internal class HirschbergAligner(
             val scoreL = nwScore(x.subList(0, xmid), y)
             val scoreR = nwScore(x.subList(xmid + 1, x.size).asReversed(), y.asReversed())
             val ymid = y.indices.minBy { scoreL[it] + scoreR[scoreR.size - it - 1] }
-            return hirschberg(x.subList(0, xmid), y.subList(0, ymid)) + hirschberg(
+            hirschberg(target, x.subList(0, xmid), y.subList(0, ymid))
+            hirschberg(
+                target,
                 if (xmid < x.size) x.subList(xmid, x.size) else emptyList(),
                 if (ymid < y.size) y.subList(ymid, y.size) else emptyList()
             )
         }
     }
 
-    private val Step.cost: Int
-        get() = when (type) {
-            DeviationType.None -> 0
-            DeviationType.LogDeviation -> logMove!!.insertionCost
-            DeviationType.ModelDeviation -> modelMove!!.deletionCost
-        }
-
     override fun align(trace: Trace, costUpperBound: Int): Alignment? {
-        val events = trace.events.toList()
-        val steps = hirschberg(model.trace, events)
-//        assert(steps.mapNotNull { it.modelMove } == model.trace) { "Not all activities were reflected in the alignments.\nModel: ${model.trace}\nTrace: ${events.map { it.conceptName }}\nAlignment: ${steps.mapNotNull { it.modelMove }}" }
-        return Alignment(steps, steps.sumOf { it.cost })
+        val steps = LinkedList<Step>()
+        val nonSilent = model.trace.filter { !it.isSilent }
+        hirschberg(steps, nonSilent, trace.events.toList())
+        val cost = steps.sumOf {
+            when (it.type) {
+                DeviationType.None -> 0
+                DeviationType.LogDeviation -> penalty.logMove
+                DeviationType.ModelDeviation -> penalty.modelMove
+            }
+        }
+        if (nonSilent.size < model.trace.size) {
+            assert(steps.mapNotNull { it.modelMove } == nonSilent) { "${steps.mapNotNull { it.modelMove }} $nonSilent" }
+            val i = steps.listIterator()
+            for (a in model.trace) {
+                if (a.isSilent) {
+                    i.add(Step(modelMove = a, logMove = null, type = DeviationType.ModelDeviation))
+                } else {
+                    var step = i.next()
+                    while (step.modelMove === null && i.hasNext()) {
+                        step = i.next()
+                    }
+                    assert(step.modelMove === a)
+                }
+            }
+        }
+        assert(steps.mapNotNull { it.modelMove } == model.trace)
+        return Alignment(steps, cost)
     }
 }
