e22a47dd80ad9807a80f4125e7e5fd607b0a0c0b
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2024-11-08 08:57:02 +0100

    chore: (processm.conformance) tidying up NeedlemanWunschAlignerTest (#328)
    Former-commit-id: 1498719fc425dc639b018508e2790647f696119f

--- processm.conformance/src/test/kotlin/processm/conformance/models/antialignments/NeedlemanWunschAlignerTest.kt
+++ processm.conformance/src/test/kotlin/processm/conformance/models/antialignments/NeedlemanWunschAlignerTest.kt
@@ -6,25 +6,13 @@ import processm.core.log.attribute.Attribute.CONCEPT_NAME
 import processm.core.log.attribute.mutableAttributeMapOf
 import processm.core.log.hierarchical.Trace
 import processm.core.models.commons.Activity
-import kotlin.test.*
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 class NeedlemanWunschAlignerTest {
     private data class MyActivity(override val name: String, override val isSilent: Boolean = false) : Activity
-//
-//    @Test
-//    fun `nwScore regression`() {
-//        val activities = "agtacgca".map { MyActivity(it.toString(), isSilent = it == '_') }
-//        val trace = Helpers.logFromString("t a t g c").traces.single()
-//        val events = trace.events.toList()
-//        with(HirschbergAligner(ReplayModel(activities))) {
-//            assertContentEquals(intArrayOf(8, 7, 6, 7, 6, 5), nwScore(activities, events))
-//            assertContentEquals(intArrayOf(4, 3, 2, 3, 4, 5), nwScore(activities.subList(0, 4), events))
-//            assertContentEquals(
-//                intArrayOf(4, 3, 2, 3, 4, 5),
-//                nwScore(activities.subList(4, activities.size).asReversed(), events.asReversed())
-//            )
-//        }
-//    }
 
     @Test
     fun test() {
@@ -54,58 +42,6 @@ class NeedlemanWunschAlignerTest {
     }
 
     @Test
-    fun wikipedia() {
-        val activities = "agtacgca".map { MyActivity(it.toString(), isSilent = it == '_') }
-        val trace = Helpers.logFromString("t a t g c").traces.single()
-        val events = trace.events.toList()
-        with(NeedlemanWunschAligner(ReplayModel(activities))) {
-            val a = this.align(trace, 999)!!
-            assertNotNull(a)
-            assertEquals(5, a.cost)
-            assertEquals(9, a.steps.size)
-            //TODO
-//            with(a.steps[0]) {
-//                assertEquals(activities[0], modelMove)
-//                assertNull(logMove)
-//            }
-//            with(a.steps[1]) {
-//                assertEquals(activities[1], modelMove)
-//                assertNull(logMove)
-//            }
-//            with(a.steps[2]) {
-//                assertEquals(activities[2], modelMove)
-//                assertEquals(events[0], logMove)
-//            }
-//            with(a.steps[3]) {
-//                assertEquals(activities[3], modelMove)
-//                assertEquals(events[1], logMove)
-//            }
-//            // The ordering of the following two steps is arbitrary. I believe the algorithm is stable,
-//            // but should the test fail check whether they did not swap places
-//            with(a.steps[4]) {
-//                assertEquals(activities[4], modelMove)
-//                assertNull(logMove)
-//            }
-//            with(a.steps[5]) {
-//                assertNull(modelMove)
-//                assertEquals(events[2], logMove)
-//            }
-//            with(a.steps[6]) {
-//                assertEquals(activities[5], modelMove)
-//                assertEquals(events[3], logMove)
-//            }
-//            with(a.steps[7]) {
-//                assertEquals(activities[6], modelMove)
-//                assertEquals(events[4], logMove)
-//            }
-//            with(a.steps[8]) {
-//                assertEquals(activities[7], modelMove)
-//                assertNull(logMove)
-//            }
-        }
-    }
-
-    @Test
     fun `missing event 1`() {
         val activities =
             "invite reviewers, _before review 1*, _before review 3*, time-out 1, time-out 3, _after review 1*, _after review 3*, _before review 2*, time-out 2, _after review 2*, collect reviews, decide, _after decide*, reject, _end*"
@@ -116,7 +52,6 @@ class NeedlemanWunschAlignerTest {
                 }
         val events =
             "invite reviewers, get review 2, time-out 1, time-out 3, collect reviews, decide, invite additional reviewer, time-out X, invite additional reviewer, time-out X, invite additional reviewer, time-out X, invite additional reviewer, time-out X, invite additional reviewer, time-out X, invite additional reviewer, time-out X, invite additional reviewer, time-out X, invite additional reviewer, get review X, invite additional reviewer, time-out X, invite additional reviewer, time-out X, invite additional reviewer, time-out X, invite additional reviewer, get review X, reject"
-//            "invite reviewers, get review 2, time-out 1, time-out 3, collect reviews, decide, reject"
                 .split(",").map {
                     Event(mutableAttributeMapOf(CONCEPT_NAME to it.trim()))
                 }
